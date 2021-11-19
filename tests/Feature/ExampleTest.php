af96f55915cf03f399b40f046a3a5ccc900c6834
Connor <connor.tumbleson@gmail.com>
2021-11-19 07:08:38 -0500

    feat: install phpcs

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,11 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function testExampleFeature(): void
     {
         $response = $this->get('/');
 
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,12 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function testExampleUnit(): void
     {
         $this->assertTrue(true);
     }
