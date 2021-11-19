7d3f1e3fa20d69f166c84f9c5dff555c1a5fdc98
Connor <connor.tumbleson@gmail.com>
2021-11-19 07:34:35 -0500

    fix: correct sqlite migration during tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,11 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use LazilyRefreshDatabase;
     use CreatesApplication;
 }
