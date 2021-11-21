5b8204a9ff1bc21faabaeec3fa2455b0f78370a4
Connor <connor.tumbleson@gmail.com>
2021-11-21 17:46:16 -0500

    feat: matches endpoint for api

--- app/Services/HaloDotApi/InfiniteInterface.php
+++ app/Services/HaloDotApi/InfiniteInterface.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 namespace App\Services\HaloDotApi;
 
 use App\Models\Player;
+use Illuminate\Database\Eloquent\Collection;
 
 interface InfiniteInterface
 {
     public function appearance(string $gamertag): ?Player;
+    public function matches(Player $player): ?Collection;
 }
