14338c3c5382385353cceb6a7831a1ccfaca9124
Connor <connor.tumbleson@gmail.com>
2021-11-21 17:47:05 -0500

    feat: cli job to pull matches

---
+++ app/Console/Commands/PullMatchHistory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Player;
+use App\Services\HaloDotApi\InfiniteInterface;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class PullMatchHistory extends Command
+{
+    protected $signature = 'app:pull-match-history {player}';
+    protected $description = 'Pull match history from API';
+
+    protected InfiniteInterface $client;
+
+    public function __construct(InfiniteInterface $client)
+    {
+        parent::__construct();
+        $this->client = $client;
+    }
+
+    public function handle(): int
+    {
+        /** @var Player|null $player */
+        $player = Player::query()->find($this->argument('player'));
+
+        if (empty($player)) {
+            $this->error('Player not found.');
+            return CommandAlias::FAILURE;
+        }
+
+        $this->client->matches($player);
+
+        return CommandAlias::SUCCESS;
+    }
+}
