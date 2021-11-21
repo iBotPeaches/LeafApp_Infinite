b5c317ee43fc1670efb25fca7f03ad2500789b15
Connor <connor.tumbleson@gmail.com>
2021-11-21 13:49:38 -0500

    fix: correct map/category models w/ database changes to fk

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int $id
  * @property string $uuid
  * @property string $version
  * @property string $name
@@ -14,4 +15,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
 }
