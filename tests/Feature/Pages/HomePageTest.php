f9eb244d65d155917c5be1c04145a5df49c5bf85
Connor <connor.tumbleson@gmail.com>
2021-11-21 10:11:27 -0500

    test: homepage test for presence of add-gamer-form

---
+++ tests/Feature/Pages/HomePageTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Feature\Pages;
+
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+    public function testExampleFeature(): void
+    {
+        // Arrange & Act
+        $response = $this->get('/');
+
+        // Assert
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertSeeLivewire('add-gamer-form');
+    }
+}
