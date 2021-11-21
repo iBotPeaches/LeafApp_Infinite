7e696311a86dac083dde7d994fa4c0e6783cbfd2
Connor <connor.tumbleson@gmail.com>
2021-11-21 07:44:31 -0500

    feat: wire up api -> model for Player

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Contracts\HasHaloDotApi;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 /**
  * @property int $id
@@ -12,7 +14,40 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $emblem_url
  * @property string $backdrop_url
  */
-class Player extends Model
+class Player extends Model implements HasHaloDotApi
 {
     use HasFactory;
+
+    public $guarded = [
+        'id'
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'gamertag';
+    }
+
+    public static function fromGamertag(string $gamertag): self
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return self::query()
+            ->where('gamertag', $gamertag)
+            ->firstOrNew([
+                'gamertag' => $gamertag
+            ]);
+    }
+
+    public static function fromHaloDotApi(array $payload): ?self
+    {
+        // @phpstan-ignore-next-line
+        $player = self::fromGamertag(Arr::get($payload, 'additional.gamertag'));
+
+        $player->fill([
+            'service_tag' => Arr::get($payload, 'data.service_tag'),
+            'emblem_url' => Arr::get($payload, 'data.emblem_url'),
+            'backdrop_url' => Arr::get($payload, 'data.backdrop_image_url')
+        ]);
+
+        return $player;
+    }
 }
