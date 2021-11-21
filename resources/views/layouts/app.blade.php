f1e09746b27513c46af33e531f40159fb41440b7
Connor <connor.tumbleson@gmail.com>
2021-11-21 07:43:13 -0500

    feat: install laravel livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,17 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <livewire:styles />
 </head>
 <!-- prevent flash of unstyled content in FF -->
 <script>0</script>
 
-<body class="bg-grey-light font-sans leading-normal tracking-normal flex flex-col min-h-screen ">
+<body class="bg-grey-light font-sans leading-normal tracking-normal flex flex-col min-h-screen">
 @include('partials.global.navigation')
 <main class="container mx-auto bg-white mt-24 md:mt-18 flex-grow">
     @yield('content')
 </main>
 @include('partials.global.footer')
+<livewire:scripts />
 </body>
 </html>
