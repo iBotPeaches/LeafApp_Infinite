2c4885a211e4dbd7cb31e41cd7b7993903ab03fc
Connor <connor.tumbleson@gmail.com>
2021-11-21 07:44:44 -0500

    feat: create rule for validating players

---
+++ app/Rules/ValidInfiniteAccount.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Rules;
+
+use App\Services\HaloDotApi\InfiniteInterface;
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidInfiniteAccount implements Rule
+{
+    private InfiniteInterface $apiClient;
+
+    public function __construct()
+    {
+        $this->apiClient = resolve(InfiniteInterface::class);
+    }
+
+    public function passes($attribute, $value): bool
+    {
+        if (empty($value)) {
+            return false;
+        }
+
+        if (is_string($value)) {
+            $player = $this->apiClient->appearance($value);
+
+            if ($player) {
+                return $player->saveQuietly();
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    public function message(): string
+    {
+        return 'This gamertag was not found in Halo Infinite.';
+    }
+}
