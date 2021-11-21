00931ecbf3313077eaf96277c700c7d2f8bd5da7
Connor <connor.tumbleson@gmail.com>
2021-11-21 17:58:51 -0500

    feat: add open source link to footer

--- resources/views/partials/global/footer.blade.php
+++ resources/views/partials/global/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="py-2 text-center">
-    Data from <a href="https://halodotapi.com">HaloDotAPI</a>
+    <a href="https://github.com/iBotPeaches/LeafApp_Infinite">Open Source</a> || Data from <a href="https://halodotapi.com">HaloDotAPI</a>
 </footer>
