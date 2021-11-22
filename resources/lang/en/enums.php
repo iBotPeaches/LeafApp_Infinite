a49741ac4386955fd41291ad24bbc0603e1be41e
Connor <connor.tumbleson@gmail.com>
2021-11-22 18:42:08 -0500

    feat: add new draw (outcome) and pve-bots (experience)

--- resources/lang/en/enums.php
+++ resources/lang/en/enums.php
@@ -6,11 +6,13 @@ use App\Enums\Outcome;
 return [
     Experience::class => [
         Experience::BTB => 'BTB',
-        Experience::ARENA => 'Arena'
+        Experience::ARENA => 'Arena',
+        Experience::PVE_BOTS => 'PVE Bots',
     ],
     Outcome::class => [
         Outcome::WIN => 'Win',
         Outcome::LOSS => 'Loss',
-        Outcome::LEFT => 'Left'
+        Outcome::LEFT => 'Left',
+        Outcome::DRAW => 'Draw',
     ]
 ];
