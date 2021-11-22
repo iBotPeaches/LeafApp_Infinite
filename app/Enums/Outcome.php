c9cd5fa58a2cff922d885722e8e6ef655d387b4a
Connor <connor.tumbleson@gmail.com>
2021-11-22 18:42:08 -0500

    feat: add new draw (outcome) and pve-bots (experience)

--- app/Enums/Outcome.php
+++ app/Enums/Outcome.php
@@ -9,10 +9,12 @@ use BenSampo\Enum\Enum;
  * @method static static WIN()
  * @method static static LOSS()
  * @method static static LEFT()
+ * @method static static DRAW()
  */
 final class Outcome extends Enum implements LocalizedEnum
 {
     const WIN = 1;
     const LOSS = 2;
     const LEFT = 3;
+    const DRAW = 4;
 }
