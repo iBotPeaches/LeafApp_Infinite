3390fac50dd09fcf76e2665fe901b94502839bd4
Connor <connor.tumbleson@gmail.com>
2021-11-22 06:59:42 -0500

    fix: hard-code page title to Leafapp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="">
 <head>
-    <title></title>
+    <title>LeafApp</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
