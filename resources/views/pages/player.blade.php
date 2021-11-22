7b2fbd17eb5fb433773fba11b5f26fa78d226d9b
Connor <connor.tumbleson@gmail.com>
2021-11-21 21:08:14 -0500

    fix: remove pre dump on profile page

--- resources/views/pages/player.blade.php
+++ resources/views/pages/player.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <pre>
-        {{ $player }}
-    </pre>
+    <div class="container mx-auto px-4 flex flex-col lg:flex-row">
+        <div class="mt-6 lg:mt-0 lg:ml-6 lg:w-1/3 p-8" style="background-image: url('{{ $player->backdrop_url }}');">
+            {{ $player->gamertag }}
+            <img src="{{ $player->emblem_url }}" />
+        </div>
+        <div class="relative lg:w-2/3 p-8">
+            TODO
+        </div>
+    </div>
 @endsection
