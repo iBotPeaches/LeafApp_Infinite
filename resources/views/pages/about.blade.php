8458027c4821bad044fd98480a85c56910119f43
Connor <connor.tumbleson@gmail.com>
2021-11-23 07:32:06 -0500

    feat: quick design pass on about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,12 @@
 @section('title', 'About')
 
 @section('content')
-    <p>
-        This application is offered by, which is solely responsible for its content. It is not sponsored or endorsed by Microsoft. This application uses HaloDotAPI, an unofficial, not sponsored or endorsed Halo API. All rights reserved. Microsoft, Halo, and the Halo Logo are trademarks of the Microsoft group of companies.
-    </p>
+    <article class="message is-link">
+        <div class="message-header">
+            <p>Legal</p>
+        </div>
+        <div class="message-body">
+            This application is offered by, which is solely responsible for its content. It is not sponsored or endorsed by Microsoft. This application uses HaloDotAPI, an unofficial, not sponsored or endorsed Halo API. All rights reserved. Microsoft, Halo, and the Halo Logo are trademarks of the Microsoft group of companies.
+        </div>
+    </article>
 @endsection
