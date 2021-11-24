52b553a4cf75fe605a33312414d550c2c4b83716
Connor <connor.tumbleson@gmail.com>
2021-11-24 06:47:10 -0500

    build: swap to deployer php, the yaml is busted with processing bugs

---
+++ deploy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/yarn.php';
+
+set('application', 'Leafapp');
+set('repository', 'git@github.com:iBotPeaches/LeafApp_Infinite.git');
+set('php_fpm_version', '8.0');
+
+host('prod')
+    ->set('remote_user', 'leafapp')
+    ->set('port', 22774)
+    ->set('hostname', 'leafapp.co')
+    ->set('deploy_path', '/home/leafapp/deploy');
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'yarn:install',
+    'yarn:run:prod',
+    'deploy:publish',
+]);
+
+task('yarn:run:prod', function () {
+    cd('{{release_or_current_path}}');
+    run('yarn run prod');
+});
+
+after('deploy:failed', 'deploy:unlock');
