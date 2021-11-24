41fe8adff8b65026ab9f1f72e2bbc21eb817722f
Connor <connor.tumbleson@gmail.com>
2021-11-24 06:42:21 -0500

    feat: allow negative scores

---
+++ database/migrations/2021_11_24_104219_fix_unsigned_nature_of_score.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class FixUnsignedNatureOfScore extends Migration
+{
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE `game_players` CHANGE `score` `score` MEDIUMINT(8) NOT NULL;');
+    }
+
+    public function down(): void
+    {
+        DB::statement('ALTER TABLE `game_players` CHANGE `score` `score` MEDIUMINT(8) UNSIGNED NOT NULL; ');
+    }
+}
