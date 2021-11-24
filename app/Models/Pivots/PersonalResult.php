4d50062130654c4796140fa1509662d4b92f001a
Connor <connor.tumbleson@gmail.com>
2021-11-24 07:09:38 -0500

    feat: add color pass for kd/kda in table

--- app/Models/Pivots/PersonalResult.php
+++ app/Models/Pivots/PersonalResult.php
@@ -39,13 +39,34 @@ class PersonalResult extends Pivot
         return number_format($value);
     }
 
-    public function hasPositiveKd(): bool
+    public function getVictoryColor(): string
     {
-        return $this->kd >= 1;
+        if ($this->outcome->is(Outcome::WIN())) {
+            return 'has-background-success-light';
+        }
+
+        if ($this->outcome->is(Outcome::LOSS())) {
+            return 'has-background-warning-light';
+        }
+
+        if ($this->outcome->is(Outcome::LEFT())) {
+            return 'has-background-danger-light';
+        }
+
+        return '';
     }
 
-    public function wasVictory(): bool
+    public function getKdColor(): string
     {
-        return $this->outcome->is(Outcome::WIN());
+        return $this->kd >= 1
+            ? 'has-background-success-light'
+            : 'has-background-danger-light';
+    }
+
+    public function getKdaColor(): string
+    {
+        return $this->kda >= 1
+            ? 'has-background-success-light'
+            : 'has-background-danger-light';
     }
 }
