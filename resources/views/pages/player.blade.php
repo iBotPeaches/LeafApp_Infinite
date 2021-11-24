67eb5d430737ceded9dd2efdbcc9ea51f6431033
Connor <connor.tumbleson@gmail.com>
2021-11-24 06:39:59 -0500

    refactor: extract player block to new component

--- resources/views/pages/player.blade.php
+++ resources/views/pages/player.blade.php
@@ -5,26 +5,8 @@
 @section('content')
     <div class="columns">
         <div class="column">
-            <div class="card has-background-light">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img src="{{ $player->backdrop_url }}">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-64x64">
-                                <img src="{{ $player->emblem_url }}">
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="title is-4">{{ $player->gamertag }}</p>
-                            <p class="subtitle is-6">{{ $player->service_tag }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('partials.player.player-card')
+            <livewire:update-player-panel :player="$player" />
         </div>
         <div class="column is-three-quarters">
             <livewire:game-history-table :player="$player" />
