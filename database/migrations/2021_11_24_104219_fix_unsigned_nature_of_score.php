03cb06f65129f573abb494031022170744eaa2b7
Connor <connor.tumbleson@gmail.com>
2021-11-24 06:54:22 -0500

    fix: force check against mysql to avoid sqlite

--- database/migrations/2021_11_24_104219_fix_unsigned_nature_of_score.php
+++ database/migrations/2021_11_24_104219_fix_unsigned_nature_of_score.php
@@ -7,15 +7,17 @@ use Illuminate\Support\Facades\Schema;
 
 class FixUnsignedNatureOfScore extends Migration
 {
-    protected $connection = 'mysql';
-
     public function up(): void
     {
-        DB::statement('ALTER TABLE `game_players` CHANGE `score` `score` MEDIUMINT(8) NOT NULL;');
+        if (DB::connection()->getDriverName() === 'mysql') {
+            DB::statement('ALTER TABLE `game_players` CHANGE `score` `score` MEDIUMINT(8) NOT NULL;');
+        }
     }
 
     public function down(): void
     {
-        DB::statement('ALTER TABLE `game_players` CHANGE `score` `score` MEDIUMINT(8) UNSIGNED NOT NULL; ');
+        if (DB::connection()->getDriverName() === 'mysql') {
+            DB::statement('ALTER TABLE `game_players` CHANGE `score` `score` MEDIUMINT(8) UNSIGNED NOT NULL;');
+        }
     }
 }
