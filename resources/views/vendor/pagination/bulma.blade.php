0d1150a963801e50073bd9dbf2162c71995aa578
Connor <connor.tumbleson@gmail.com>
2021-11-24 06:39:44 -0500

    fix: correct pagination to use livewire

--- resources/views/vendor/pagination/bulma.blade.php
+++ resources/views/vendor/pagination/bulma.blade.php
@@ -1,18 +1,20 @@
-@if ($paginator->hasPages())
-    <nav class="pagination is-centered">
-        @if ($paginator->onFirstPage())
-            <a class="pagination-previous" disabled>Previous</a>
-        @else
-            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="pagination-previous">Previous</a>
-        @endif
+<div>
+    @if ($paginator->hasPages())
+        @php(isset($this->numberOfPaginatorsRendered[$paginator->getPageName()]) ? $this->numberOfPaginatorsRendered[$paginator->getPageName()]++ : $this->numberOfPaginatorsRendered[$paginator->getPageName()] = 1)
 
-        @if ($paginator->hasMorePages())
-            <a class="pagination-next" href="{{ $paginator->nextPageUrl() }}" rel="next">Next</a>
-        @else
-            <a class="pagination-next" disabled>Next page</a>
-        @endif
+        <nav class="pagination is-centered">
+            @if ($paginator->onFirstPage())
+                <a class="pagination-previous" disabled>Previous</a>
+            @else
+                <a class="pagination-previous" wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="prev" aria-label="@lang('pagination.previous')">Previous</a>
+            @endif
 
-        @if (isset($elements))
+            @if ($paginator->hasMorePages())
+                <a class="pagination-next" wire:click="nextPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="next" aria-label="@lang('pagination.next')">Next</a>
+            @else
+                <a class="pagination-next" disabled>Next page</a>
+            @endif
+
             <ul class="pagination-list">
                 @foreach ($elements as $element)
                     @if (is_string($element))
@@ -22,15 +24,14 @@
                     @if (is_array($element))
                         @foreach ($element as $page => $url)
                             @if ($page == $paginator->currentPage())
-                                <li><a class="pagination-link is-current">{{ $page }}</a></li>
+                                <li wire:key="paginator-{{ $paginator->getPageName() }}-{{ $this->numberOfPaginatorsRendered[$paginator->getPageName()] }}-dead-page-{{ $page }}"><span class="pagination-link is-current" aria-current="page">{{ $page }}</span></li>
                             @else
-                                <li><a href="{{ $url }}" class="pagination-link">{{ $page }}</a></li>
+                                <li wire:key="paginator-{{ $paginator->getPageName() }}-{{ $this->numberOfPaginatorsRendered[$paginator->getPageName()] }}-page-{{ $page }}"><span class="pagination-link" wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')">{{ $page }}</span></li>
                             @endif
                         @endforeach
                     @endif
                 @endforeach
             </ul>
-        @endif
-    </nav>
-    <br />
-@endif
+        </nav>
+    @endif
+</div>
