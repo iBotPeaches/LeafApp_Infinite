378cff07b2e2297ce5e81dea274f1b19f55e414a
Connor <connor.tumbleson@gmail.com>
2021-11-26 06:55:56 -0500

    feat: update api class for pulling competitive data

--- app/Services/HaloDotApi/InfiniteInterface.php
+++ app/Services/HaloDotApi/InfiniteInterface.php
@@ -3,11 +3,13 @@ declare(strict_types=1);
 
 namespace App\Services\HaloDotApi;
 
+use App\Models\Csr;
 use App\Models\Player;
 use Illuminate\Database\Eloquent\Collection;
 
 interface InfiniteInterface
 {
     public function appearance(string $gamertag): ?Player;
+    public function competitive(Player $player): ?Csr;
     public function matches(Player $player, bool $forceUpdate = false): ?Collection;
 }
