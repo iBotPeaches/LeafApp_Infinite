c0725e300d3eebb465e5cdf12f80404a4d62b2d3
Connor <connor.tumbleson@gmail.com>
2021-11-26 06:56:39 -0500

    feat: new enums for Input/Queue

---
+++ app/Enums/Input.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Contracts\LocalizedEnum;
+use BenSampo\Enum\Enum;
+use Illuminate\Support\Str;
+
+/**
+ * @method static static CROSSPLAY()
+ * @method static static CONTROLLER()
+ * @method static static KBM()
+ */
+final class Input extends Enum implements LocalizedEnum
+{
+    const CROSSPLAY = 0;
+    const CONTROLLER = 1;
+    const KBM = 2;
+
+    public static function coerce($enumKeyOrValue): ?Enum
+    {
+        // The HaloDotAPI uses 'mnk', I prefer 'kbm'
+        if ($enumKeyOrValue === 'mnk') {
+            $enumKeyOrValue = 'kbm';
+        }
+
+        $enumKeyOrValue = Str::upper(Str::replace('-', '_', $enumKeyOrValue));
+        return parent::coerce($enumKeyOrValue);
+    }
+}
