fe64ce87273dfdb0fcdb49304af59e6a3e11905d
Connor <connor.tumbleson@gmail.com>
2021-11-26 06:56:39 -0500

    feat: new enums for Input/Queue

---
+++ app/Enums/Queue.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Contracts\LocalizedEnum;
+use BenSampo\Enum\Enum;
+use Illuminate\Support\Str;
+
+/**
+ * @method static static OPEN()
+ * @method static static SOLO_DUO()
+ */
+final class Queue extends Enum implements LocalizedEnum
+{
+    const OPEN = 0;
+    const SOLO_DUO = 1;
+
+    public static function coerce($enumKeyOrValue): ?Enum
+    {
+        $enumKeyOrValue = Str::upper(Str::replace('-', '_', $enumKeyOrValue));
+        return parent::coerce($enumKeyOrValue);
+    }
+}
