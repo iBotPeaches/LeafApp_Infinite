2b1c6dc47624af98b470018bc20051a9b2724207
Connor <connor.tumbleson@gmail.com>
2021-11-27 13:10:56 -0500

    test: factory/tests for CSR model

---
+++ database/factories/CsrFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\Input;
+use App\Enums\Queue;
+use App\Models\Csr;
+use App\Models\Player;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
+
+/**
+ * @method Collection|Csr[]|Csr create($attributes = [], ?Csr $parent = null)
+ * @method Collection|Csr[] createMany(iterable $records)
+ * @method Csr createOne($attributes = [])
+ * @method Collection|Csr[]|Csr make($attributes = [], ?Csr $parent = null)
+ * @method Csr makeOne($attributes = [])
+ */
+class CsrFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'player_id' => Player::factory(),
+            'queue' => Queue::getRandomValue(),
+            'input' => Input::getRandomValue(),
+            'season' => 1,
+            'csr' => 1225,
+            'matches_remaining' => 0,
+            'tier' => 'Diamond',
+            'tier_image_url' => $this->faker->imageUrl,
+            'sub_tier' => 1,
+            'next_tier' => 'Diamond',
+            'next_sub_tier' => 5,
+            'next_csr' => 1500,
+            'season_tier' => 'Diamond',
+            'season_sub_tier' => 4,
+        ];
+    }
+}
