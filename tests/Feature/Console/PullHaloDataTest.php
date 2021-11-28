c8f289c62af3b8028bd3fda37a1e617d9f9c80e4
Connor <connor.tumbleson@gmail.com>
2021-11-28 17:00:47 -0500

    test: add assertions for console tests

---
+++ tests/Feature/Console/PullHaloDataTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Feature\Console;
+
+use App\Models\Player;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Mocks\Csrs\MockCsrAllService;
+use Tests\Mocks\Matches\MockMatchesService;
+use Tests\Mocks\ServiceRecord\MockServiceRecordService;
+use Tests\TestCase;
+
+class PullHaloDataTest extends TestCase
+{
+    use WithFaker;
+
+    public function testInvalidGamertag(): void
+    {
+        $this
+            ->artisan('app:pull-halo-data', ['player' => '999999999'])
+            ->assertExitCode(CommandAlias::FAILURE);
+    }
+
+    public function testValidDataPull(): void
+    {
+        // Arrange
+        $gamertag = $this->faker->word . $this->faker->numerify;
+        $mockCsrResponse = (new MockCsrAllService())->success($gamertag);
+        $mockMatchesResponse = (new MockMatchesService())->success($gamertag);
+        $mockServiceResponse = (new MockServiceRecordService())->success($gamertag);
+
+        Http::fakeSequence()
+            ->push($mockCsrResponse, Response::HTTP_OK)
+            ->push($mockMatchesResponse, Response::HTTP_OK)
+            ->push($mockServiceResponse, Response::HTTP_OK);
+
+        $player = Player::factory()->createOne([
+            'gamertag' => $gamertag
+        ]);
+
+        // Act & Assert
+        $this
+            ->artisan('app:pull-halo-data', ['player' => $player->id])
+            ->assertExitCode(CommandAlias::SUCCESS);
+    }
+}
