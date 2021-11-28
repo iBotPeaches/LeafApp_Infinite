01abbd7371de25a5ade648a59a49cb4a7b3bb630
Connor <connor.tumbleson@gmail.com>
2021-11-28 06:40:20 -0500

    refactor: build up support for better http mocks

--- tests/Mocks/Appearance/MockAppearanceService.php
+++ tests/Mocks/Appearance/MockAppearanceService.php
@@ -3,11 +3,13 @@ declare(strict_types=1);
 
 namespace Tests\Mocks\Appearance;
 
-use Symfony\Component\HttpFoundation\Response;
 use Tests\Mocks\BaseMock;
+use Tests\Mocks\Traits\HasErrorFunctions;
 
 class MockAppearanceService extends BaseMock
 {
+    use HasErrorFunctions;
+
     public function success(): array
     {
         return [
@@ -19,51 +21,6 @@ class MockAppearanceService extends BaseMock
             'additional' => [
                 'gamertag' => $this->faker->word
             ]
-        ];
-    }
-
-    public function error401(): array
-    {
-        return [
-            'statusCode' => Response::HTTP_UNAUTHORIZED,
-            'error' => 'Unauthorized',
-            'message' => 'Missing Authentication'
-        ];
-    }
-
-    public function error403(): array
-    {
-        return [
-            'statusCode' => Response::HTTP_FORBIDDEN,
-            'error' => 'Forbidden',
-            'message' => 'Forbidden'
-        ];
-    }
-
-    public function error404(): array
-    {
-        return [
-            'statusCode' => Response::HTTP_NOT_FOUND,
-            'error' => 'Not Found',
-            'message' => 'Not Found'
-        ];
-    }
-
-    public function error429(): array
-    {
-        return [
-            'statusCode' => Response::HTTP_TOO_MANY_REQUESTS,
-            'error' => 'Too Many Requests',
-            'message' => 'Rate limit reached, please retry in 343 second(s)'
-        ];
-    }
-
-    public function error500(): array
-    {
-        return [
-            'statusCode' => Response::HTTP_INTERNAL_SERVER_ERROR,
-            'error' => 'Internal Server Error',
-            'message' => 'Something went wrong...'
         ];
     }
 }
--- tests/Mocks/Matches/MockMatchesService.php
+++ tests/Mocks/Matches/MockMatchesService.php
@@ -4,94 +4,99 @@ declare(strict_types=1);
 namespace Tests\Mocks\Matches;
 
 use Tests\Mocks\BaseMock;
+use Tests\Mocks\Traits\HasErrorFunctions;
 
 class MockMatchesService extends BaseMock
 {
+    use HasErrorFunctions;
+
     public function success(int $page = 1, int $nextPage = 1): array
     {
         return [
             'data' => [
-                'id' => $this->faker->uuid,
-                'details' => [
-                    'category' => [
-                        'name' => $this->faker->word,
-                        'asset' => [
-                            'id' => $this->faker->uuid,
-                            'version' => $this->faker->uuid,
-                            'thumbnail_url' => $this->faker->url
-                        ]
-                    ],
-                    'map' => [
-                        'name' => $this->faker->word,
-                        'asset' => [
-                            'id' => $this->faker->uuid,
-                            'version' => $this->faker->uuid,
-                            'thumbnail_url' => $this->faker->url
-                        ]
-                    ]
-                ],
-                'teams' => [
-                    'enabled' => $this->faker->boolean,
-                    'scoring' => $this->faker->boolean,
+                [
+                    'id' => $this->faker->uuid,
                     'details' => [
-                        'id' => $this->faker->numerify('#'),
-                        'name' => $this->faker->word,
-                        'emblem_url' => $this->faker->url
-                    ]
-                ],
-                'stats' => [
-                    'summary' => [
-                        'kills' => $this->faker->numberBetween(1, 25),
-                        'deaths' => $this->faker->numberBetween(1, 25),
-                        'assists' => $this->faker->numberBetween(1, 25),
-                        'betrayals' => $this->faker->numberBetween(0, 5),
-                        'suicides' => $this->faker->numberBetween(0, 5),
-                        'vehicles' => [
-                            'destroys' => $this->faker->numberBetween(0, 2),
-                            'hijacks' => $this->faker->numberBetween(0, 2),
+                        'category' => [
+                            'name' => $this->faker->word,
+                            'asset' => [
+                                'id' => $this->faker->uuid,
+                                'version' => $this->faker->uuid,
+                                'thumbnail_url' => $this->faker->url
+                            ]
                         ],
-                        'medals' => $this->faker->numberBetween(1, 25)
-                    ],
-                    'damage' => [
-                        'taken' => $this->faker->numerify('####'),
-                        'dealt' => $this->faker->numerify('####')
-                    ],
-                    'shots' => [
-                        'fired' => $this->faker->numerify(),
-                        'landed' => $this->faker->numerify(),
-                        'missed' => $this->faker->numerify(),
-                        'accuracy' => $this->faker->randomFloat(2, 0, 100)
-                    ],
-                    'rounds' => [
-                        'won' => $this->faker->numberBetween(0, 2),
-                        'lost' => $this->faker->numberBetween(0, 2),
-                        'tied' => $this->faker->numberBetween(0, 2),
-                    ],
-                    'breakdowns' => [
-                        'kills' => [
-                            'melee' => $this->faker->numberBetween(0, 5),
-                            'grenades' => $this->faker->numberBetween(0, 5),
-                            'headshots' => $this->faker->numberBetween(0, 5),
-                            'power_weapons' => $this->faker->numberBetween(0, 5),
-                        ],
-                        'assists' => [
-                            'emp' => $this->faker->numberBetween(0, 5),
-                            'driver' => $this->faker->numberBetween(0, 5),
-                            'callouts' => $this->faker->numberBetween(0, 5)
+                        'map' => [
+                            'name' => $this->faker->word,
+                            'asset' => [
+                                'id' => $this->faker->uuid,
+                                'version' => $this->faker->uuid,
+                                'thumbnail_url' => $this->faker->url
+                            ]
                         ]
                     ],
-                    'kda' => $this->faker->randomFloat(2, 0, 10),
-                    'kdr' => $this->faker->randomFloat(2, 0, 10),
-                    'score' => $this->faker->numerify('####')
-                ],
-                'rank' => $this->faker->numerify('#'),
-                'outcome' => $this->faker->randomElement(['win', 'lost', 'tied']),
-                'experience' => $this->faker->randomElement(['arena', 'btb']),
-                'ranked' => $this->faker->boolean,
-                'played_at' => $this->faker->dateTime,
-                'duration' => [
-                    'seconds' => $this->faker->numerify('###'),
-                    'human' => ''
+                    'teams' => [
+                        'enabled' => $this->faker->boolean,
+                        'scoring' => $this->faker->boolean,
+                        'details' => [
+                            'id' => $this->faker->numerify('#'),
+                            'name' => $this->faker->word,
+                            'emblem_url' => $this->faker->url
+                        ]
+                    ],
+                    'stats' => [
+                        'summary' => [
+                            'kills' => $this->faker->numberBetween(1, 25),
+                            'deaths' => $this->faker->numberBetween(1, 25),
+                            'assists' => $this->faker->numberBetween(1, 25),
+                            'betrayals' => $this->faker->numberBetween(0, 5),
+                            'suicides' => $this->faker->numberBetween(0, 5),
+                            'vehicles' => [
+                                'destroys' => $this->faker->numberBetween(0, 2),
+                                'hijacks' => $this->faker->numberBetween(0, 2),
+                            ],
+                            'medals' => $this->faker->numberBetween(1, 25)
+                        ],
+                        'damage' => [
+                            'taken' => $this->faker->numerify('####'),
+                            'dealt' => $this->faker->numerify('####')
+                        ],
+                        'shots' => [
+                            'fired' => $this->faker->numerify(),
+                            'landed' => $this->faker->numerify(),
+                            'missed' => $this->faker->numerify(),
+                            'accuracy' => $this->faker->randomFloat(2, 0, 100)
+                        ],
+                        'rounds' => [
+                            'won' => $this->faker->numberBetween(0, 2),
+                            'lost' => $this->faker->numberBetween(0, 2),
+                            'tied' => $this->faker->numberBetween(0, 2),
+                        ],
+                        'breakdowns' => [
+                            'kills' => [
+                                'melee' => $this->faker->numberBetween(0, 5),
+                                'grenades' => $this->faker->numberBetween(0, 5),
+                                'headshots' => $this->faker->numberBetween(0, 5),
+                                'power_weapons' => $this->faker->numberBetween(0, 5),
+                            ],
+                            'assists' => [
+                                'emp' => $this->faker->numberBetween(0, 5),
+                                'driver' => $this->faker->numberBetween(0, 5),
+                                'callouts' => $this->faker->numberBetween(0, 5)
+                            ]
+                        ],
+                        'kda' => $this->faker->randomFloat(2, 0, 10),
+                        'kdr' => $this->faker->randomFloat(2, 0, 10),
+                        'score' => $this->faker->numerify('####')
+                    ],
+                    'rank' => $this->faker->numerify('#'),
+                    'outcome' => $this->faker->randomElement(['win', 'lost', 'tied']),
+                    'experience' => $this->faker->randomElement(['arena', 'btb']),
+                    'ranked' => $this->faker->boolean,
+                    'played_at' => $this->faker->dateTime,
+                    'duration' => [
+                        'seconds' => $this->faker->numerify('###'),
+                        'human' => ''
+                    ]
                 ]
             ],
             'count' => 25,
@@ -101,7 +106,8 @@ class MockMatchesService extends BaseMock
                 'next' => $nextPage,
             ],
             'additional' => [
-                'gamertag' => $this->faker->word
+                'gamertag' => $this->faker->word,
+                'mode' => 'matchmade'
             ]
         ];
     }
