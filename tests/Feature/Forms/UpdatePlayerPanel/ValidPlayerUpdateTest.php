950428ca61642ff64e006e8196875c17b08bebe4
Connor <connor.tumbleson@gmail.com>
2021-11-28 11:06:35 -0500

    test: assertions for update player panel

---
+++ tests/Feature/Forms/UpdatePlayerPanel/ValidPlayerUpdateTest.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Feature\Forms\UpdatePlayerPanel;
+
+use App\Enums\PlayerTab;
+use App\Http\Livewire\CompetitivePage;
+use App\Http\Livewire\GameHistoryTable;
+use App\Http\Livewire\OverviewPage;
+use App\Http\Livewire\UpdatePlayerPanel;
+use App\Models\Player;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Livewire\Livewire;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Mocks\Csrs\MockCsrAllService;
+use Tests\Mocks\Matches\MockMatchesService;
+use Tests\Mocks\ServiceRecord\MockServiceRecordService;
+use Tests\TestCase;
+
+class ValidPlayerUpdateTest extends TestCase
+{
+    use WithFaker;
+
+    public function testInitialPageLoadDeferredFromApiCalls(): void
+    {
+        // Arrange
+        Http::fake();
+        $player = Player::factory()->createOne();
+
+        // Act & Assert
+        Livewire::test(UpdatePlayerPanel::class, [
+            'player' => $player,
+            'type' => PlayerTab::OVERVIEW,
+            'runUpdate' => false
+        ])
+            ->call('render')
+            ->assertViewHas('color', 'is-info')
+            ->assertViewHas('message', 'Checking for updated stats.');
+
+        Http::assertNothingSent();
+    }
+
+    /** @dataProvider validPageDataProvider */
+    public function testValidResponseFromAllHaloDotApiServices(string $type, string $event): void
+    {
+        // Arrange
+        $gamertag = $this->faker->word . $this->faker->numerify;
+        $mockCsrResponse = (new MockCsrAllService())->success($gamertag);
+        $mockMatchesResponse = (new MockMatchesService())->success($gamertag);
+        $mockServiceResponse = (new MockServiceRecordService())->success($gamertag);
+
+        Http::fakeSequence()
+            ->push($mockCsrResponse, Response::HTTP_OK)
+            ->push($mockMatchesResponse, Response::HTTP_OK)
+            ->push($mockServiceResponse, Response::HTTP_OK);
+
+        $player = Player::factory()->createOne([
+            'gamertag' => $gamertag
+        ]);
+
+        // Act & Assert
+        Livewire::test(UpdatePlayerPanel::class, [
+            'player' => $player,
+            'type' => $type,
+            'runUpdate' => true
+        ])
+            ->assertViewHas('color', 'is-success')
+            ->assertViewHas('message', 'Profile updated!')
+
+            // TODO - I cannot assert to a specific component - https://github.com/livewire/livewire/discussions/4298
+            ->assertEmitted('$refresh');
+    }
+
+    public function validPageDataProvider(): array
+    {
+        return [
+            'overview' => [
+                'type' => PlayerTab::OVERVIEW,
+                'event' => OverviewPage::class,
+            ],
+            'competitive' => [
+                'type' => PlayerTab::COMPETITIVE,
+                'event' => CompetitivePage::class,
+            ],
+            'matches' => [
+                'type' => PlayerTab::MATCHES,
+                'event' => GameHistoryTable::class,
+            ]
+        ];
+    }
+}
