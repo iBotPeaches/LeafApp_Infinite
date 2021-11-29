b18a2cb67a0d7e95ef5843a09376836ace758dcf
Connor <connor.tumbleson@gmail.com>
2021-11-28 19:42:25 -0500

    test: assert all possibilities of playlist win percent

--- tests/Feature/Forms/CompetitivePage/ValidCompetitivePageTest.php
+++ tests/Feature/Forms/CompetitivePage/ValidCompetitivePageTest.php
@@ -20,13 +20,31 @@ class ValidCompetitivePageTest extends TestCase
         Carbon::setTestNow(now());
         $player = Player::factory()
             ->has(Csr::factory()->state(function () {
-                return ['queue' => Queue::OPEN, 'input' => Input::CROSSPLAY];
+                return [
+                    'queue' => Queue::OPEN,
+                    'input' => Input::CROSSPLAY,
+                    'next_csr' => 1500,
+                    'tier_start_csr' => 1450,
+                    'csr' => 1499
+                ];
             }))
             ->has(Csr::factory()->state(function () {
-                return ['queue' => Queue::SOLO_DUO, 'input' => Input::KBM];
+                return [
+                    'queue' => Queue::SOLO_DUO,
+                    'input' => Input::KBM,
+                    'next_csr' => 1500,
+                    'tier_start_csr' => 1450,
+                    'csr' => 1480
+                ];
             }))
             ->has(Csr::factory()->state(function () {
-                return ['queue' => Queue::SOLO_DUO, 'input' => Input::CONTROLLER];
+                return [
+                    'queue' => Queue::SOLO_DUO,
+                    'input' => Input::CONTROLLER,
+                    'next_csr' => 1500,
+                    'tier_start_csr' => 1450,
+                    'csr' => 1485
+                ];
             }))
             ->createOne();
 
@@ -47,6 +65,53 @@ class ValidCompetitivePageTest extends TestCase
             $livewire->assertSee($playlist->tier_image_url);
             $livewire->assertSee($playlist->rank);
             $livewire->assertSee(number_format($playlist->csr));
+        }
+    }
+
+    public function testValidUnratedResponseFromHaloDotApi(): void
+    {
+        // Arrange
+        Carbon::setTestNow(now());
+        $player = Player::factory()
+            ->has(Csr::factory()->state(function () {
+                return [
+                    'queue' => Queue::OPEN,
+                    'input' => Input::CROSSPLAY,
+                    'matches_remaining' => 10,
+                    'next_csr' => 0,
+                    'tier' => 'Unrated',
+                    'tier_start_csr' => 0,
+                    'csr' => null
+                ];
+            }))
+            ->has(Csr::factory()->state(function () {
+                return [
+                    'queue' => Queue::SOLO_DUO,
+                    'input' => Input::CONTROLLER,
+                    'next_csr' => 1500,
+                    'tier_start_csr' => 1450,
+                    'csr' => 1450
+                ];
+            }))
+            ->createOne();
+
+        /** @var Csr[] $ranked */
+        $ranked = $player->ranked(1);
+
+        // Act
+        $livewire = Livewire::test(CompetitivePage::class, [
+            'player' => $player
+        ]);
+
+        // Assert
+        $livewire->assertViewHas('ranked');
+
+        foreach ($ranked as $playlist) {
+            $livewire->assertSee($playlist->title);
+            $livewire->assertSee($playlist->icon, false);
+            $livewire->assertSee($playlist->tier_image_url);
+            $livewire->assertSee($playlist->rank);
+            $livewire->assertSee('In Placements');
         }
     }
 }
