29b5c6205fd74e5da78190f694bad56c26570bb2
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-06 06:51:12 -0500

    test: assertion for Diamond 6 -> Onyx

--- tests/Unit/Models/CsrModelTest.php
+++ tests/Unit/Models/CsrModelTest.php
@@ -57,6 +57,14 @@ class CsrModelTest extends TestCase
                 'next_sub_tier' => 1,
                 'expected' => 'Diamond 2'
             ],
+            'diamond 6' => [
+                'tier' => 'Diamond',
+                'sub_tier' => 5,
+                'next_csr' => 1500,
+                'next_tier' => 'Onyx',
+                'next_sub_tier' => 0,
+                'expected' => 'Onyx'
+            ],
             'onyx' => [
                 'tier' => 'Onyx',
                 'sub_tier' => 0,
