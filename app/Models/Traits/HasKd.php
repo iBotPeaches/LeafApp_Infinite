1efd7efedcd9c565df6c85881fb6c9882cbe0dcf
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-11 16:42:24 -0500

    refactor: split PersonalResult logic to trait for re-use

---
+++ app/Models/Traits/HasKd.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models\Traits;
+
+use function number_format;
+
+trait HasKd
+{
+    public function getKdAttribute(float $value): string
+    {
+        return number_format($value, 2);
+    }
+
+    public function getKdaAttribute(float $value): string
+    {
+        return number_format($value, 2);
+    }
+
+    public function getKdColor(): string
+    {
+        return $this->kd >= 1
+            ? 'has-background-success-light'
+            : 'has-background-danger-light';
+    }
+
+    public function getKdaColor(): string
+    {
+        return $this->kda >= 1
+            ? 'has-background-success-light'
+            : 'has-background-danger-light';
+    }
+}
