ad9db207ed10c17a4175554e2d307cb91542d45c
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-11 16:51:54 -0500

    fix: support for pulling specific match

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use App\Enums\Experience;
 use App\Models\Contracts\HasHaloDotApi;
 use App\Models\Pivots\PersonalResult;
+use App\Services\Autocode\Enums\PlayerType;
+use App\Services\Autocode\InfiniteInterface;
 use Carbon\Carbon;
 use Database\Factories\GameFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Arr;
 
 /**
@@ -26,6 +30,8 @@ use Illuminate\Support\Arr;
  * @property-read Category $category
  * @property-read Map $map
  * @property-read PersonalResult $personal
+ * @property-read GamePlayer[]|Collection $players
+ * @property-read GameTeam[]|Collection $teams
  * @method static GameFactory factory(...$parameters)
  */
 class Game extends Model implements HasHaloDotApi
@@ -53,6 +59,11 @@ class Game extends Model implements HasHaloDotApi
 
     public $timestamps = false;
 
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+
     public function setExperienceAttribute(string $value): void
     {
         $experience = is_numeric($value) ? Experience::fromValue((int) $value) : Experience::coerce($value);
@@ -61,6 +72,19 @@ class Game extends Model implements HasHaloDotApi
         }
 
         $this->attributes['experience'] = $experience->value;
+    }
+
+    public function updateFromHaloDotApi(): void
+    {
+        /** @var InfiniteInterface $client */
+        $client = resolve(InfiniteInterface::class);
+
+        $client->match($this->uuid);
+    }
+
+    public function findTeamFromId(int $id): ?GameTeam
+    {
+        return $this->teams->firstWhere('internal_team_id', $id);
     }
 
     public static function fromHaloDotApi(array $payload): ?self
@@ -89,6 +113,30 @@ class Game extends Model implements HasHaloDotApi
             $game->saveOrFail();
         }
 
+        if (Arr::has($payload, 'teams.details')) {
+            foreach (Arr::get($payload, 'teams.details', []) as $teamData) {
+                $teamData['_leaf']['game'] = $game;
+                GameTeam::fromHaloDotApi($teamData);
+            }
+        }
+
+        if (Arr::has($payload, 'players')) {
+            foreach (Arr::get($payload, 'players', []) as $playerData) {
+                // TODO - No idea if non-players are in games.
+                if (Arr::get($playerData, 'type', PlayerType::PLAYER) !== PlayerType::PLAYER) {
+                    continue;
+                }
+
+                $player = Player::fromGamertag(Arr::get($playerData, 'gamertag'));
+                if (! $player->exists) {
+                    $player->saveOrFail();
+                }
+                $playerData['_leaf']['player'] = $player;
+                $playerData['_leaf']['game'] = $game;
+                GamePlayer::fromHaloDotApi($playerData);
+            }
+        }
+
         return $game;
     }
 
@@ -100,5 +148,16 @@ class Game extends Model implements HasHaloDotApi
     public function map(): BelongsTo
     {
         return $this->belongsTo(Map::class);
+    }
+
+    public function players(): HasMany
+    {
+        return $this->hasMany(GamePlayer::class);
+    }
+
+    public function teams(): HasMany
+    {
+        return $this->hasMany(GameTeam::class)
+            ->orderBy('rank');
     }
 }
