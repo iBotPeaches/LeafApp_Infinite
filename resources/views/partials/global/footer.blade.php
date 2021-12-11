1f3fac97e3f30709d8f946a3129228e44b7f4dc2
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-11 07:01:09 -0500

    fix: footer adjustment for autocode

--- resources/views/partials/global/footer.blade.php
+++ resources/views/partials/global/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="content has-text-centered">
         <p>
             <strong>Leaf</strong> by <a href="https://twitter.com/iBotPeaches">iBotPeaches</a>. The source code is available at
-            <a href="https://github.com/iBotPeaches/LeafApp_Infinite">GitHub</a>. API Data from <a href="https://halodotapi.com">HaloDotAPI</a>
+            <a href="https://github.com/iBotPeaches/LeafApp_Infinite">GitHub</a>. API Data from <a href="https://autocode.com/lib/halo/">HaloDotAPI (Autocode)</a>
         </p>
     </div>
 </footer>
