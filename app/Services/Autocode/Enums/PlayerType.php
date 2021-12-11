44fabdc28ab82b19a83a0e56f3c040789baa3f68
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-11 16:50:33 -0500

    feat: enum for bot vs player

---
+++ app/Services/Autocode/Enums/PlayerType.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services\Autocode\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static PLAYER()
+ * @method static static BOT()
+ */
+final class PlayerType extends Enum
+{
+    const PLAYER = 'player';
+    const BOT = 'bot';
+}
