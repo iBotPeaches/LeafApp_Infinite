a74f9995b85039ca40040ce36e202fb8bdaba167
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-11 16:50:18 -0500

    feat: initial design of post game carnage report

---
+++ resources/views/partials/game/game-card.blade.php
@@ -0,0 +1,28 @@
+<?php
+/** @var App\Models\Game $game */
+?>
+<div class="card mb-3">
+    <div class="card-image">
+        <figure class="image is-4by3">
+            <img src="{{ $game->map->thumbnail_url }}" alt="{{ $game->map->name }}">
+        </figure>
+    </div>
+    <div class="card-content">
+        <div class="media">
+            <div class="media-content">
+                <p class="title is-4">{{ $game->map->name }}</p>
+                <p class="subtitle is-6">{{ $game->category->name }}</p>
+            </div>
+        </div>
+        <div class="content">
+            {{ $game->experience->description }}
+            @if ($game->is_ranked)
+                <abbr title="Ranked"><i class="fa fa-crosshairs"></i></abbr>
+            @endif
+            <br />
+            <time datetime="{{ $game->occurred_at->toIso8601ZuluString() }}">
+                {{ $game->occurred_at->diffForHumans() }}
+            </time>
+        </div>
+    </div>
+</div>
