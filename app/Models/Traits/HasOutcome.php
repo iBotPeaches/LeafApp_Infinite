8ae1d9b66609a5e23986d12b63a81b03055f3930
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-11 16:49:39 -0500

    refactor: move outcome into trait

---
+++ app/Models/Traits/HasOutcome.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models\Traits;
+
+use App\Enums\Outcome;
+use Illuminate\Support\Str;
+use function is_numeric;
+
+trait HasOutcome
+{
+    public function setOutcomeAttribute(string $value): void
+    {
+        $outcome = is_numeric($value) ? Outcome::fromValue((int) $value) : Outcome::coerce(Str::upper($value));
+        if (empty($outcome)) {
+            throw new \InvalidArgumentException('Invalid Outcome Enum (' . $value . ')');
+        }
+
+        $this->attributes['outcome'] = $outcome->value;
+    }
+}
