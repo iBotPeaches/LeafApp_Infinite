25dcdd5a2297c0e450bd90792816b8db4e82ff67
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-11 16:49:39 -0500

    refactor: move outcome into trait

--- app/Models/GamePlayer.php
+++ app/Models/GamePlayer.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use App\Enums\Outcome;
 use App\Models\Contracts\HasHaloDotApi;
+use App\Models\Traits\HasKd;
+use App\Models\Traits\HasOutcome;
+use App\Models\Traits\HasScoring;
 use Database\Factories\GamePlayerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 
 /**
  * @property int $id
@@ -46,11 +48,12 @@ use Illuminate\Support\Str;
  * @property int $assists_callout
  * @property-read Player $player
  * @property-read Game $game
+ * @property-read GameTeam $team
  * @method static GamePlayerFactory factory(...$parameters)
  */
 class GamePlayer extends Model implements HasHaloDotApi
 {
-    use HasFactory;
+    use HasFactory, HasOutcome, HasKd, HasScoring;
 
     public $casts = [
         'outcome' => Outcome::class
@@ -61,16 +64,6 @@ class GamePlayer extends Model implements HasHaloDotApi
     public $with = [
         'game'
     ];
-
-    public function setOutcomeAttribute(string $value): void
-    {
-        $outcome = is_numeric($value) ? Outcome::fromValue((int) $value) : Outcome::coerce(Str::upper($value));
-        if (empty($outcome)) {
-            throw new \InvalidArgumentException('Invalid Outcome Enum (' . $value . ')');
-        }
-
-        $this->attributes['outcome'] = $outcome->value;
-    }
 
     public static function fromHaloDotApi(array $payload): ?self
     {
@@ -86,37 +79,45 @@ class GamePlayer extends Model implements HasHaloDotApi
             ->where('game_id', $game->id)
             ->firstOrNew();
 
+        // We use this function between two calls. The match history endpoint & the match retrieve endpoint.
+        // The individual match endpoint returns all players, so it keyed by player, thus doesn't need "player"
+        $prefix = Arr::has($payload, 'player') ? 'player.' : null;
+        if (empty($prefix)) {
+            $team = $game->findTeamFromId(Arr::get($payload, 'team.id'));
+            $gamePlayer->team()->associate($team);
+        }
+
         $gamePlayer->player()->associate($player);
         $gamePlayer->game()->associate($game);
-        $gamePlayer->rank = Arr::get($payload, 'player.rank');
-        $gamePlayer->outcome = Arr::get($payload, 'player.outcome');
-        $gamePlayer->kd = Arr::get($payload, 'player.stats.core.kdr');
-        $gamePlayer->kda = Arr::get($payload, 'player.stats.core.kda');
-        $gamePlayer->score = Arr::get($payload, 'player.stats.core.score');
-        $gamePlayer->kills = Arr::get($payload, 'player.stats.core.summary.kills');
-        $gamePlayer->deaths = Arr::get($payload, 'player.stats.core.summary.deaths');
-        $gamePlayer->assists = Arr::get($payload, 'player.stats.core.summary.assists');
-        $gamePlayer->betrayals = Arr::get($payload, 'player.stats.core.summary.betrayals');
-        $gamePlayer->suicides = Arr::get($payload, 'player.stats.core.summary.suicides');
-        $gamePlayer->vehicle_destroys = Arr::get($payload, 'player.stats.core.summary.vehicles.destroys');
-        $gamePlayer->vehicle_hijacks = Arr::get($payload, 'player.stats.core.summary.vehicles.hijacks');
-        $gamePlayer->medal_count = Arr::get($payload, 'player.stats.core.summary.medals');
-        $gamePlayer->damage_taken = Arr::get($payload, 'player.stats.core.damage.taken');
-        $gamePlayer->damage_dealt = Arr::get($payload, 'player.stats.core.damage.dealt');
-        $gamePlayer->shots_fired = Arr::get($payload, 'player.stats.core.shots.fired');
-        $gamePlayer->shots_landed = Arr::get($payload, 'player.stats.core.shots.landed');
-        $gamePlayer->shots_missed = Arr::get($payload, 'player.stats.core.shots.missed');
-        $gamePlayer->accuracy = Arr::get($payload, 'player.stats.core.shots.accuracy');
-        $gamePlayer->rounds_won = Arr::get($payload, 'player.stats.core.rounds.won');
-        $gamePlayer->rounds_lost = Arr::get($payload, 'player.stats.core.rounds.lost');
-        $gamePlayer->rounds_tied = Arr::get($payload, 'player.stats.core.rounds.tied');
-        $gamePlayer->kills_melee = Arr::get($payload, 'player.stats.core.breakdowns.kills.melee');
-        $gamePlayer->kills_grenade = Arr::get($payload, 'player.stats.core.breakdowns.kills.grenades');
-        $gamePlayer->kills_headshot = Arr::get($payload, 'player.stats.core.breakdowns.kills.headshots');
-        $gamePlayer->kills_power = Arr::get($payload, 'player.stats.core.breakdowns.kills.power_weapons');
-        $gamePlayer->assists_emp = Arr::get($payload, 'player.stats.core.breakdowns.assists.emp');
-        $gamePlayer->assists_driver = Arr::get($payload, 'player.stats.core.breakdowns.assists.driver');
-        $gamePlayer->assists_callout = Arr::get($payload, 'player.stats.core.breakdowns.assists.callouts');
+        $gamePlayer->rank = Arr::get($payload, $prefix . 'rank');
+        $gamePlayer->outcome = Arr::get($payload, $prefix . 'outcome');
+        $gamePlayer->kd = Arr::get($payload, $prefix . 'stats.core.kdr');
+        $gamePlayer->kda = Arr::get($payload, $prefix . 'stats.core.kda');
+        $gamePlayer->score = Arr::get($payload, $prefix . 'stats.core.score');
+        $gamePlayer->kills = Arr::get($payload, $prefix . 'stats.core.summary.kills');
+        $gamePlayer->deaths = Arr::get($payload, $prefix . 'stats.core.summary.deaths');
+        $gamePlayer->assists = Arr::get($payload, $prefix . 'stats.core.summary.assists');
+        $gamePlayer->betrayals = Arr::get($payload, $prefix . 'stats.core.summary.betrayals');
+        $gamePlayer->suicides = Arr::get($payload, $prefix . 'stats.core.summary.suicides');
+        $gamePlayer->vehicle_destroys = Arr::get($payload, $prefix . 'stats.core.summary.vehicles.destroys');
+        $gamePlayer->vehicle_hijacks = Arr::get($payload, $prefix . 'stats.core.summary.vehicles.hijacks');
+        $gamePlayer->medal_count = Arr::get($payload, $prefix . 'stats.core.summary.medals');
+        $gamePlayer->damage_taken = Arr::get($payload, $prefix . 'stats.core.damage.taken');
+        $gamePlayer->damage_dealt = Arr::get($payload, $prefix . 'stats.core.damage.dealt');
+        $gamePlayer->shots_fired = Arr::get($payload, $prefix . 'stats.core.shots.fired');
+        $gamePlayer->shots_landed = Arr::get($payload, $prefix . 'stats.core.shots.landed');
+        $gamePlayer->shots_missed = Arr::get($payload, $prefix . 'stats.core.shots.missed');
+        $gamePlayer->accuracy = Arr::get($payload, $prefix . 'stats.core.shots.accuracy');
+        $gamePlayer->rounds_won = Arr::get($payload, $prefix . 'stats.core.rounds.won');
+        $gamePlayer->rounds_lost = Arr::get($payload, $prefix . 'stats.core.rounds.lost');
+        $gamePlayer->rounds_tied = Arr::get($payload, $prefix . 'stats.core.rounds.tied');
+        $gamePlayer->kills_melee = Arr::get($payload, $prefix . 'stats.core.breakdowns.kills.melee');
+        $gamePlayer->kills_grenade = Arr::get($payload, $prefix . 'stats.core.breakdowns.kills.grenades');
+        $gamePlayer->kills_headshot = Arr::get($payload, $prefix . 'stats.core.breakdowns.kills.headshots');
+        $gamePlayer->kills_power = Arr::get($payload, $prefix . 'stats.core.breakdowns.kills.power_weapons');
+        $gamePlayer->assists_emp = Arr::get($payload, $prefix . 'stats.core.breakdowns.assists.emp');
+        $gamePlayer->assists_driver = Arr::get($payload, $prefix . 'stats.core.breakdowns.assists.driver');
+        $gamePlayer->assists_callout = Arr::get($payload, $prefix . 'stats.core.breakdowns.assists.callouts');
 
         if ($gamePlayer->isDirty()) {
             $gamePlayer->saveOrFail();
@@ -133,5 +134,10 @@ class GamePlayer extends Model implements HasHaloDotApi
     public function game(): BelongsTo
     {
         return $this->belongsTo(Game::class);
+    }
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(GameTeam::class, 'game_team_id');
     }
 }
