ccbcde9c4cfe205c31c7f4ecd47d855826030989
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-12 15:39:41 -0500

    feat: add factory for GameTeam

---
+++ database/factories/GameTeamFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\GameTeam;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
+
+/**
+ * @method Collection|GameTeam[]|GameTeam create($attributes = [], ?GameTeam $parent = null)
+ * @method Collection|GameTeam[] createMany(iterable $records)
+ * @method GameTeam createOne($attributes = [])
+ * @method Collection|GameTeam[]|GameTeam make($attributes = [], ?GameTeam $parent = null)
+ * @method GameTeam makeOne($attributes = [])
+ */
+class GameTeamFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
