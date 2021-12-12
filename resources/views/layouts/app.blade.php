4964c4e244c36f35053ef2158273fd4acc94bf78
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-12 17:08:13 -0500

    fix: correct description still using LeafApp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html class="has-navbar-fixed-top" lang="en">
 <head>
     <title>Leaf @yield('title')</title>
-    <meta name="description" content="LeafApp @yield('description')" />
+    <meta name="description" content="Leaf @yield('description')" />
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
