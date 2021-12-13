dd3c249e58dbd3824858d7ad031b1b2958a3dbf6
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-13 07:03:48 -0500

    test: support looking for bot user during game pull

--- tests/Feature/Forms/UpdateGamePanel/ValidGameUpdateTest.php
+++ tests/Feature/Forms/UpdateGamePanel/ValidGameUpdateTest.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\UpdateGamePanel;
 use App\Jobs\PullAppearance;
 use App\Jobs\PullXuid;
 use App\Models\Game;
+use App\Services\Autocode\Enums\PlayerType;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Bus;
@@ -38,6 +39,37 @@ class ValidGameUpdateTest extends TestCase
             ->assertViewHas('message', 'Checking for missing players.');
 
         Http::assertNothingSent();
+    }
+
+    public function testValidResponseFromAllHaloDotApiServicesForABot(): void
+    {
+        // Arrange
+        Queue::fake();
+        $gamertag = $this->faker->word . $this->faker->numerify;
+        $gamertag2 = $this->faker->word . $this->faker->numerify;
+        $mockMatchResponse = (new MockMatchService())->success($gamertag, $gamertag2);
+
+        Arr::set($mockMatchResponse, 'data.players.1.type', PlayerType::BOT);
+
+        Http::fakeSequence()
+            ->push($mockMatchResponse, Response::HTTP_OK);
+
+        $game = Game::factory()->createOne([
+            'uuid' => Arr::get($mockMatchResponse, 'data.id')
+        ]);
+
+        // Act & Assert
+        Livewire::test(UpdateGamePanel::class, [
+            'game' => $game,
+            'runUpdate' => true
+        ])
+            ->assertViewHas('color', 'is-success')
+            ->assertViewHas('message', 'Game updated!')
+
+            // TODO - I cannot assert to a specific component - https://github.com/livewire/livewire/discussions/4298
+            ->assertEmitted('$refresh');
+
+        Queue::assertPushed(PullAppearance::class);
     }
 
     public function testValidResponseFromAllHaloDotApiServices(): void
