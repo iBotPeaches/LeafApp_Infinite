110b31d04ab3745a1a89579762df6cdd5016ea36
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-13 07:01:55 -0500

    refactor: move xuid pull into job

---
+++ app/Jobs/PullXuid.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Models\Player;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class PullXuid implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private Player $player;
+
+    public function __construct(Player $player)
+    {
+        $this->player = $player;
+    }
+
+    public function handle(): void
+    {
+        $this->player->syncXuidFromXboxApi();
+        if ($this->player->isDirty('xuid')) {
+            $this->player->saveOrFail();
+        }
+    }
+}
