af5b0963a8d9a5c20fc5b487e0b6157d9e19711a
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-13 21:18:38 -0500

    fix: correct issue if no team data on pgcr

--- resources/views/partials/game/team_emblem_url.blade.php
+++ resources/views/partials/game/team_emblem_url.blade.php
@@ -2,7 +2,9 @@
 /** @var App\Models\GamePlayer $gamePlayer */
 ?>
 @if (empty($gamePlayer->player->emblem_url))
-    <img class="is-rounded has-background-dark" src="{{ $gamePlayer->team->emblem_url }}">
+    @if ($gamePlayer->team)
+        <img class="is-rounded has-background-dark" src="{{ $gamePlayer->team->emblem_url }}">
+    @endif
 @else
     <img src="{{ $gamePlayer->player->emblem_url }}">
 @endif
