524896ce436b5858a1017bad9c9e72de6a379d3b
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-14 05:50:50 -0500

    fix: bump to autocode 0.1.2

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'autocode' => [
         'key' => env('AUTOCODE_KEY'),
         'domain' => env('AUTOCODE_DOMAIN', 'https://halo.api.stdlib.com'),
-        'version' => env('AUTOCODE_VERSION', '0.1.1'),
+        'version' => env('AUTOCODE_VERSION', '0.1.2'),
         'cooldown' => env('AUTOCODE_COOLDOWN', 120),
     ],
 
