dd646fea5a25121afe7d39ec2f27a8f0ce4c13a6
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-15 07:06:18 -0500

    feat: db changes for new playlist property + participation

---
+++ database/migrations/2021_12_15_120331_add_partipation_columns.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPartipationColumns extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('game_players', function (Blueprint $table) {
+            $table->boolean('was_at_start')
+                ->after('outcome')
+                ->default(true);
+
+            $table->boolean('was_at_end')
+                ->after('was_at_start')
+                ->default(true);
+
+            $table->boolean('was_inprogress_join')
+                ->after('was_at_end')
+                ->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('game_players', function (Blueprint $table) {
+            $table->dropColumn([
+                'was_at_start',
+                'was_at_end',
+                'was_inprogress_join'
+            ]);
+        });
+    }
+}
