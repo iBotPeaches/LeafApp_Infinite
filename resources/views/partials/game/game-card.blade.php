097a7f4e8740862c38d9dec4acb582ea861801f4
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-16 06:44:32 -0500

    refactor: playlist is the way

--- resources/views/partials/game/game-card.blade.php
+++ resources/views/partials/game/game-card.blade.php
@@ -20,16 +20,9 @@
             </div>
         </div>
         <div class="content">
-            @if ($game->playlist)
-                {{ $game->playlist->name }}
-                @if ($game->playlist->is_ranked)
-                    <abbr title="Ranked"><i class="fa fa-crosshairs"></i></abbr>
-                @endif
-            @else
-                {{ $game->experience->description }}
-                @if ($game->is_ranked)
-                    <abbr title="Ranked"><i class="fa fa-crosshairs"></i></abbr>
-                @endif
+            {{ $game->playlist->name }}
+            @if ($game->playlist->is_ranked)
+                <abbr title="Ranked"><i class="fa fa-crosshairs"></i></abbr>
             @endif
             <br />
             <time datetime="{{ $game->occurred_at->toIso8601ZuluString() }}">
