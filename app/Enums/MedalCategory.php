ff09d0f3d61d2f575aae95f1beb0984bc9be3083
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-25 09:00:10 -0500

    feat: new enums for medal category/type

---
+++ app/Enums/MedalCategory.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Contracts\LocalizedEnum;
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static MODE()
+ * @method static static MULTIKILL()
+ * @method static static PROFICIENCY()
+ * @method static static SKILL()
+ * @method static static STYLE()
+ */
+final class MedalCategory extends Enum implements LocalizedEnum
+{
+    const MODE = 1;
+    const MULTIKILL = 2;
+    const PROFICIENCY = 3;
+    const SKILL = 4;
+    const STYLE = 5;
+}
