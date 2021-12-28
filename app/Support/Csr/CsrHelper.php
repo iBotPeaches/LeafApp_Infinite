674e6d13cc7c2c214ccf060475efd19c6f0b49ad
Connor Tumbleson <connor.tumbleson@gmail.com>
2021-12-28 06:50:36 -0500

    feat: csr helper from numeric to class

---
+++ app/Support/Csr/CsrHelper.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Support\Csr;
+
+class CsrHelper
+{
+    public static function getCsrFromValue(int $value): Csr
+    {
+        // Since there is 50 CSR per level and 6 levels per class
+        $rankClass = $value / 50;
+        $subTier = ceil($rankClass + 0.001);
+
+        switch (true) {
+            case $rankClass > 0 && $rankClass < 6:
+                return new Csr($value, 'Bronze ' . $subTier);
+
+            case $rankClass >= 6 && $rankClass < 12:
+                return new Csr($value, 'Silver ' . ($subTier - 6));
+
+            case $rankClass >= 12 && $rankClass < 18:
+                return new Csr($value, 'Gold ' . ($subTier - 12));
+
+            case $rankClass >= 18 && $rankClass < 24:
+                return new Csr($value, 'Platinum ' . ($subTier - 18));
+
+            case $rankClass >= 24 && $rankClass < 30:
+                return new Csr($value, 'Diamond ' . ($subTier - 24));
+
+            case $rankClass >= 30:
+                return new Csr($value, 'Onyx');
+
+            default:
+                return new Csr(0, 'Unranked');
+        }
+    }
+}
