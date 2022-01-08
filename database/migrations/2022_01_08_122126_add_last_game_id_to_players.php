320296e85bf07331feab17748f3ce3948a278fa5
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-08 07:36:19 -0500

    feat: add last_game_id_pulled to players

---
+++ database/migrations/2022_01_08_122126_add_last_game_id_to_players.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Game;
+
+class AddLastGameIdToPlayers extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table
+                ->foreignIdFor(Game::class, 'last_game_id_pulled')
+                ->after('is_private')
+                ->nullable(true)
+                ->constrained('games', 'id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('last_game_id_pulled');
+        });
+    }
+}
