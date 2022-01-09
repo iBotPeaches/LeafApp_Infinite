48fa51013b54d7cc69e997e0716ae865a523a8e2
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-09 10:26:03 -0500

    test: assert that slayer gamemode is tested

--- tests/Feature/Forms/UpdateGamePanel/ValidGameUpdateTest.php
+++ tests/Feature/Forms/UpdateGamePanel/ValidGameUpdateTest.php
@@ -135,6 +135,37 @@ class ValidGameUpdateTest extends TestCase
         Queue::assertPushed(PullAppearance::class);
     }
 
+    public function testValidResponseFromAllHaloDotApiServicesAsSlayer(): void
+    {
+        // Arrange
+        Queue::fake();
+        $gamertag = $this->faker->word . $this->faker->numerify;
+        $gamertag2 = $this->faker->word . $this->faker->numerify;
+        $mockMatchResponse = (new MockMatchService())->success($gamertag, $gamertag2);
+
+        Arr::set($mockMatchResponse, 'data.details.category.name', 'Slayer');
+
+        Http::fakeSequence()
+            ->push($mockMatchResponse, Response::HTTP_OK);
+
+        $game = Game::factory()->createOne([
+            'uuid' => Arr::get($mockMatchResponse, 'data.id')
+        ]);
+
+        // Act & Assert
+        Livewire::test(UpdateGamePanel::class, [
+            'game' => $game,
+            'runUpdate' => true
+        ])
+            ->assertViewHas('color', 'is-success')
+            ->assertViewHas('message', 'Game updated!')
+
+            // TODO - I cannot assert to a specific component - https://github.com/livewire/livewire/discussions/4298
+            ->assertEmitted('$refresh');
+
+        Queue::assertPushed(PullAppearance::class);
+    }
+
     public function testValidResponseFromAllHaloDotApiServicesAsCtf(): void
     {
         // Arrange
