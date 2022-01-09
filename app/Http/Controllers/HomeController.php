a11cb3d7b72c3f2fcdeda686f158ed4967171b05
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-09 10:20:44 -0500

    feat: add last updated to controller for home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,23 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
 use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
     public function index(): View
     {
-        return view('pages.home');
+        $lastUpdated = Player::query()
+            ->with('serviceRecord')
+            ->whereHas('serviceRecord')
+            ->orderByDesc('updated_at')
+            ->limit(5)
+            ->get();
+
+        return view('pages.home', [
+            'lastUpdated' => $lastUpdated
+        ]);
     }
 
     public function about(): View
