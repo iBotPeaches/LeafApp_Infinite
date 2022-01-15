94ff630482ab4496c003723656cc69ca5eb918d0
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-15 17:06:16 -0500

    feat: add seo for player/game page

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
+use Artesaos\SEOTools\Facades\SEOTools;
 
 class GameController extends Controller
 {
     public function index(Game $game): View
     {
+        SEOTools::setTitle($game->name);
+        SEOTools::addImages([
+            $game->category->thumbnail_url,
+            $game->map->thumbnail_url,
+            $game->playlist->thumbnail_url
+        ]);
+        SEOTools::setDescription($game->description);
+
         return view('pages.game', [
             'game' => $game,
         ]);
