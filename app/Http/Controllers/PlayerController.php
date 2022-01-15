d9a87233a193235afde81019deac4845a553a2a9
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-15 17:06:16 -0500

    feat: add seo for player/game page

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers;
 
 use App\Enums\PlayerTab;
 use App\Models\Player;
+use Artesaos\SEOTools\Facades\SEOTools;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class PlayerController extends Controller
 {
     public function index(Player $player, string $type = PlayerTab::OVERVIEW): View
     {
+        SEOTools::setTitle($player->gamertag . ' ' . Str::title($type));
+        SEOTools::addImages([
+            $player->emblem_url
+        ]);
+        SEOTools::setDescription($player->gamertag . ' Halo Infinite ' . Str::title($type));
+
         return view('pages.player', [
             'player' => $player,
             'type' => $type
