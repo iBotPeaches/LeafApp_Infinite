ed9cd3e08fe6e74a1cc4eb4bd779dec622a7de78
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-15 17:05:56 -0500

    feat: install seotools

---
+++ config/seotools.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    'meta' => [
+        'defaults'       => [
+            'title'        => "",
+            'titleBefore'  => false,
+            'description'  => '',
+            'separator'    => ' - ',
+            'keywords'     => [],
+            'canonical'    => false,
+            'robots'       => false,
+        ],
+        'webmaster_tags' => [
+            'google'    => null,
+            'bing'      => null,
+            'alexa'     => null,
+            'pinterest' => null,
+            'yandex'    => null,
+            'norton'    => null,
+        ],
+
+        'add_notranslate_class' => false,
+    ],
+    'opengraph' => [
+        'defaults' => [
+            'title'       => '',
+            'description' => '',
+            'url'         => false,
+            'type'        => false,
+            'site_name'   => false,
+            'images'      => [],
+        ],
+    ],
+    'twitter' => [
+        'defaults' => [
+        ],
+    ],
+    'json-ld' => [
+        'defaults' => [
+            'title'       => '',
+            'description' => '',
+            'url'         => false,
+            'type'        => 'WebPage',
+            'images'      => [],
+        ],
+    ],
+];
