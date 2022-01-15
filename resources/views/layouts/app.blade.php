f33331a9b20775807f529591285e74d0c14a54cd
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-15 17:06:06 -0500

    fix: wire up seotools to template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="description" content="@yield('description')" />
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    {!! Artesaos\SEOTools\Facades\SEOTools::generate() !!}
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <livewire:styles />
 </head>
