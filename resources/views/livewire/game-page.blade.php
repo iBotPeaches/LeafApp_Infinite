e13933c98ea88df67ca06ee07a7b5502c3a99411
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2022-01-15 21:43:37 -0500

    fix: don't show powerful medals if game is outdated/not-pulled (#162)

--- resources/views/livewire/game-page.blade.php
+++ resources/views/livewire/game-page.blade.php
@@ -15,7 +15,9 @@
         @foreach ($groupedGamePlayers as $gamePlayers)
             @include('partials.game.team-table')
         @endforeach
-        <div class="divider">Powerful Medals (Legendary or Heroic)</div>
-        @include('partials.game.powerful_medals')
+        @if (! $game->outdated)
+            <div class="divider">Powerful Medals (Legendary or Heroic)</div>
+            @include('partials.game.powerful_medals')
+        @endif
     </div>
 </div>
