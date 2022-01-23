49a5eebe738876b063813048dc1a4834df2c3bc9
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-23 15:55:08 -0500

    feat: console command to pull championship

---
+++ app/Console/Commands/PullChampionship.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Console\Commands;
+
+use App\Services\FaceIt\TournamentInterface;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class PullChampionship extends Command
+{
+    protected $signature = 'app:championship {championshipId}';
+    protected $description = 'Pulls Championship from FaceIt';
+
+    protected TournamentInterface $client;
+
+    public function __construct(TournamentInterface $client)
+    {
+        parent::__construct();
+        $this->client = $client;
+    }
+
+    public function handle(): int
+    {
+        $championshipId = $this->argument('championshipId');
+
+        $this->client->championship($championshipId);
+
+        return CommandAlias::SUCCESS;
+    }
+}
