df0fc64fda0568d6a2c8dd3bd4dd30fe3cbd0c7a
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-23 17:08:28 -0500

    fix: add other regions

--- app/Services/FaceIt/Enums/Region.php
+++ app/Services/FaceIt/Enums/Region.php
@@ -3,14 +3,19 @@ declare(strict_types=1);
 
 namespace App\Services\FaceIt\Enums;
 
+use BenSampo\Enum\Contracts\LocalizedEnum;
 use BenSampo\Enum\Enum;
 
 /**
  * @method static static NA()
  * @method static static EU()
+ * @method static static OCE()
+ * @method static static LATAM()
  */
-final class Region extends Enum
+final class Region extends Enum implements LocalizedEnum
 {
     const NA = 1;
     const EU = 2;
+    const OCE = 3;
+    const LATAM = 4;
 }
