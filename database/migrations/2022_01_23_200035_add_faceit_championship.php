816dbbf683acfce77f1ddcad7cb98b7985adf468
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-23 15:44:12 -0500

    feat: add championship model + migration

---
+++ database/migrations/2022_01_23_200035_add_faceit_championship.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFaceitChampionship extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('championships', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('faceit_id')->unique();
+            $table->string('name', 64);
+            $table->tinyInteger('region');
+            $table->dateTime('started_at');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('championships');
+    }
+}
