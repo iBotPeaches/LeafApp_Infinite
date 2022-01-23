34700f80f9539343be5d5090e0a435d8e2a469cf
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-23 17:16:12 -0500

    feat: add championship livewire module

---
+++ resources/views/livewire/championships-table.blade.php
@@ -0,0 +1,28 @@
+<?php
+/** @var App\Models\Championship[] $championships */
+?>
+<div>
+    <table class="table is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+        <tr>
+            <th>Championship</th>
+            <th>Region</th>
+            <th>Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($championships as $championship)
+            <tr>
+                <td>
+                    <a href="{{ route('championship', [$championship]) }}">
+                        {{ $championship->name }}
+                    </a>
+                </td>
+                <td>{{ $championship->region->description }}</td>
+                <td>{{ $championship->started_at->toDateString() }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $championships->links() }}
+</div>
