4a9c6899b4cb1dc23205ced237f13e895da1520a
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-23 15:44:40 -0500

    feat: helper trait/class for Championship (FaceIt)

---
+++ app/Services/FaceIt/Enums/Region.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services\FaceIt\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static NA()
+ */
+final class Region extends Enum
+{
+    const NA = 1;
+}
