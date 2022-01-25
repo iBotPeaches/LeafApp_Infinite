ec74937abd84bd85c5c8186c713458a7d8d1c19e
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-25 07:32:21 -0500

    feat: add loading of individual championship page

--- app/Http/Controllers/HcsController.php
+++ app/Http/Controllers/HcsController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Enums\Bracket;
 use App\Models\Championship;
 use Illuminate\Contracts\View\View;
 
@@ -13,10 +14,12 @@ class HcsController extends Controller
         return view('pages.championships');
     }
 
-    public function championship(Championship $championship): View
+    public function championship(Championship $championship, string $bracket = Bracket::WINNERS, int $round = 1): View
     {
         return view('pages.championship', [
-            'championship' => $championship
+            'championship' => $championship,
+            'bracket' => $bracket,
+            'round' => $round
         ]);
     }
 }
