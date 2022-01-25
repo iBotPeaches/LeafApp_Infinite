b35e2eb5d8863730fc7c53816a38c16f8e19b020
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-25 07:31:43 -0500

    feat: wire up livewire for bracket

--- resources/views/pages/championship.blade.php
+++ resources/views/pages/championship.blade.php
@@ -3,4 +3,9 @@
 @section('description', $championship->name)
 
 @section('content')
+    <livewire:championship-bracket
+        :championship="$championship"
+        :bracket="$bracket"
+        :round="$round"
+    ></livewire:championship-bracket>
 @endsection
