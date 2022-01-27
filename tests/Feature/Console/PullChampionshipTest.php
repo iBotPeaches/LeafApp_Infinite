551935473f80c3b37ecca66c185f2470c13b883b
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-27 07:50:14 -0500

    test: support console test for pull championship

--- tests/Feature/Console/PullChampionshipTest.php
+++ tests/Feature/Console/PullChampionshipTest.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Console;
 
+use App\Jobs\FindPlayersFromTeam;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Queue;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 use Symfony\Component\HttpFoundation\Response;
+use Tests\Mocks\Championship\MockChampionshipBracketService;
 use Tests\Mocks\Championship\MockChampionshipService;
 use Tests\TestCase;
 
@@ -17,14 +20,22 @@ class PullChampionshipTest extends TestCase
     public function testValidDataPull(): void
     {
         // Arrange
+        Queue::fake();
         $championshipId = $this->faker->uuid;
         $mockChampionshipResponse = (new MockChampionshipService())->success();
+        $mockChampionshipBracketResponse = (new MockChampionshipBracketService())->success();
+        $mockChampionshipBracketEmpty = (new MockChampionshipBracketService())->empty();
 
-        Http::fakeSequence()->push($mockChampionshipResponse, Response::HTTP_OK);
+        Http::fakeSequence()
+            ->push($mockChampionshipResponse, Response::HTTP_OK)
+            ->push($mockChampionshipBracketResponse, Response::HTTP_OK)
+            ->push($mockChampionshipBracketEmpty, Response::HTTP_OK);
 
         // Act & Assert
         $this
             ->artisan('app:championship ' . $championshipId)
             ->assertExitCode(CommandAlias::SUCCESS);
+
+        Queue::assertPushed(FindPlayersFromTeam::class);
     }
 }
---
+++ tests/Mocks/Championship/MockChampionshipBracketService.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Mocks\Championship;
+
+use App\Services\FaceIt\Enums\Region;
+use Tests\Mocks\BaseMock;
+
+class MockChampionshipBracketService extends BaseMock
+{
+    public function success(): array
+    {
+        return [
+            'items' => [
+                $this->matchBlock(),
+                $this->matchBlock()
+            ]
+        ];
+    }
+
+    public function empty(): array
+    {
+        return [
+            'items' => []
+        ];
+    }
+
+    private function playerBlock(): array
+    {
+        return [
+            'player_id' => $this->faker->uuid,
+            'nickname' => $this->faker->word,
+            'avatar' => '',
+            'membership' => 'free',
+            'game_player_id' => $this->faker->word,
+            'game_player_name' => $this->faker->word,
+            'game_skill_level' => $this->faker->numberBetween(0, 9),
+            'anticheat_required' => false
+        ];
+    }
+
+    private function teamBlock(): array
+    {
+        return [
+            'faction_id' => $this->faker->uuid,
+            'leader' => $this->faker->uuid,
+            'avatar' => $this->faker->imageUrl,
+            'roster' => [
+                $this->playerBlock(),
+                $this->playerBlock(),
+                $this->playerBlock(),
+                $this->playerBlock()
+            ],
+            'substituted' => false,
+            'name' => $this->faker->word,
+            'type' => 'premade'
+        ];
+    }
+
+    private function matchBlock(): array
+    {
+        return [
+            'match_id' => '1-' . $this->faker->uuid,
+            'version' => 2,
+            'game' => 'halo_infinite',
+            'region' => Region::getRandomValue(),
+            'competition_id' => $this->faker->uuid,
+            'competition_type' => 'championship',
+            'competition_name' => $this->faker->word,
+            'organizer_id' => $this->faker->uuid,
+            'teams' => [
+                'faction1' => $this->teamBlock(),
+                'faction2' => $this->teamBlock()
+            ],
+            'calculate_elo' => true,
+            'started_at' => now()->timestamp,
+            'finished_at' => now()->timestamp,
+            'chat_room_id' => 'match-1-' . $this->faker->uuid,
+            'best_of' => $this->faker->randomElement([3, 5, 7]),
+            'results' => [
+                'winner' => 'faction1',
+                'score' => [
+                    'faction1' => 3,
+                    'faction2' => 2
+                ]
+            ],
+            'status' => 'FINISHED',
+            'round' => $this->faker->numberBetween(1, 12),
+            'group' => $this->faker->numberBetween(1, 3),
+            'faceit_url' => $this->faker->url
+        ];
+    }
+}
