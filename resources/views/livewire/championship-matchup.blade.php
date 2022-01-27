da86c8b5d749cea273404d6ff97a61ba1452337b
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-27 07:31:04 -0500

    refactor: use blue/red for matchup

--- resources/views/livewire/championship-matchup.blade.php
+++ resources/views/livewire/championship-matchup.blade.php
@@ -2,16 +2,18 @@
 /** @var App\Models\Championship $championship */
 /** @var App\Models\Matchup $matchup */
 ?>
+<h1 class="title">{{ $championship->name }}</h1>
+<h2 class="subtitle">{{ $matchup->bracket->description }} Round {{ $matchup->round }}</h2>
 <div class="columns">
     <div class="column">
         @include('partials.hcs.matchup_team_snippet', [
-            'color' => 'is-success',
+            'color' => 'danger',
             'team' => $matchup->winner
         ])
     </div>
     <div class="column">
         @include('partials.hcs.matchup_team_snippet', [
-            'color' => 'is-warning',
+            'color' => $matchup->loser->isBye() ? 'dark' : 'info',
             'team' => $matchup->loser
         ])
     </div>
