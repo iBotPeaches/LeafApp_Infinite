df3b37fb0544412ef1ec07164edc6adc842f38c9
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-27 07:31:04 -0500

    refactor: use blue/red for matchup

--- resources/views/partials/hcs/matchup_team_snippet.blade.php
+++ resources/views/partials/hcs/matchup_team_snippet.blade.php
@@ -2,13 +2,21 @@
 /** @var string $color */
 /** @var App\Models\MatchupTeam $team */
 ?>
-<article class="message {{ $color }}">
+<article class="message is-{{ $color }}">
     <div class="message-body">
-        {{ $team->name }}
+        <strong>{{ $team->name }}</strong>
+        <span class="is-pulled-right">
+            <span class="tag is-{{ $color }}">{{ $team->points }}</span>
+        </span>
     </div>
 </article>
+@if ($team->isBye())
+    <div class="message-body">
+        This match was a bye. No games played.
+    </div>
+@endif
 @foreach ($team->players as $player)
-    <div class="card">
+    <div class="card has-background-{{ $color }}-light">
         <div class="card-content">
             <div class="media">
                 <div class="media-left">
@@ -17,8 +25,12 @@
                     </figure>
                 </div>
                 <div class="media-content">
-                    <p class="title is-4">{{ $player->gamertag ?? '' }}</p>
-                    <p class="subtitle is-6">1.0</p>
+                    <p class="title is-4">
+                        <a href="{{ route('player', [$player]) }}">
+                            {{ $player->gamertag ?? '' }}
+                        </a>
+                    </p>
+                    <p class="subtitle is-6"><i>-</i></p>
                 </div>
             </div>
         </div>
