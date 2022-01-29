e740dc1f2750acf6c85322085912ed599a1bf2f8
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-29 09:22:07 -0500

    fix: add under construction message

--- resources/views/pages/championship.blade.php
+++ resources/views/pages/championship.blade.php
@@ -8,4 +8,13 @@
         :bracket="$bracket"
         :round="$round"
     ></livewire:championship-bracket>
+    <br />
+    <article class="message is-warning">
+        <div class="message-header">
+            <p>Under Development</p>
+        </div>
+        <div class="message-body">
+            This feature is under development still. Feedback? Twitter or GitHub
+        </div>
+    </article>
 @endsection
--- resources/views/pages/matchup.blade.php
+++ resources/views/pages/matchup.blade.php
@@ -7,4 +7,13 @@
         :championship="$championship"
         :matchup="$matchup"
     ></livewire:championship-matchup>
+    <br />
+    <article class="message is-warning">
+        <div class="message-header">
+            <p>Under Development</p>
+        </div>
+        <div class="message-body">
+            This feature is under development still. Feedback? Twitter or GitHub
+        </div>
+    </article>
 @endsection
