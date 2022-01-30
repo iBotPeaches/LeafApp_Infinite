901d40d2449b31b226a306013bf51ad87c3e24b7
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-30 14:49:22 -0500

    feat: add hcs seo for matchup/conference

--- app/Http/Controllers/HcsController.php
+++ app/Http/Controllers/HcsController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Enums\Bracket;
 use App\Models\Championship;
 use App\Models\Matchup;
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Contracts\View\View;
 
 class HcsController extends Controller
@@ -17,6 +18,9 @@ class HcsController extends Controller
 
     public function championship(Championship $championship, string $bracket = Bracket::WINNERS, int $round = 1): View
     {
+        SEOTools::setTitle($championship->name);
+        SEOTools::setDescription($championship->name . ' (' . $championship->region  . ')');
+
         return view('pages.championship', [
             'championship' => $championship,
             'bracket' => $bracket,
@@ -26,6 +30,9 @@ class HcsController extends Controller
 
     public function matchup(Championship $championship, Matchup $matchup): View
     {
+        SEOTools::setTitle($matchup->title);
+        SEOTools::setDescription($matchup->description);
+
         return view('pages.matchup', [
             'championship' => $championship,
             'matchup' => $matchup
