3771c20634ae1d7645042f77e221668ee64b5424
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-30 13:11:26 -0500

    feat: controller for faceit webhooks

---
+++ app/Http/Controllers/Webhook/FaceItController.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Http\Controllers\Webhook;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\IncomingFaceItRequest;
+use App\Models\Championship;
+use App\Services\FaceIt\TournamentInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
+
+class FaceItController extends Controller
+{
+    public function __invoke(IncomingFaceItRequest $request, TournamentInterface $client): JsonResponse
+    {
+        $payload = $request->input();
+
+        if (Arr::get($payload, 'payload.entity.type') !== 'championship') {
+            return response()->json(null);
+        }
+
+        $championshipId = Arr::get($payload, 'payload.entity.id');
+
+        $championship = Championship::query()->firstWhere('faceit_id', $championshipId);
+        if (! $championship) {
+            $championship = $client->championship($championshipId);
+        }
+
+        if ($championship) {
+            $matchId = Arr::get($payload, 'payload.id');
+            $matchup = $client->matchup($championship, $matchId);
+            return response()->json($matchup?->toArray());
+        }
+
+        return response()->json(null);
+    }
+}
