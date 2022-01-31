9c1f117d59ecc13db80241e5dbe82c224b6c5910
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-01-31 06:42:29 -0500

    feat: support two different webhook types

---
+++ app/Services/FaceIt/Enums/WebhookEvent.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services\FaceIt\Enums;
+
+use BenSampo\Enum\Contracts\LocalizedEnum;
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static MATCH_STATUS_FINISHED()
+ * @method static static CHAMPIONSHIP_FINISHED()
+ */
+final class WebhookEvent extends Enum implements LocalizedEnum
+{
+    const MATCH_STATUS_FINISHED = 'match_status_finished';
+    const CHAMPIONSHIP_FINISHED = 'championship_finished';
+}
