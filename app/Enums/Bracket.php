a5c2e71227928990118bae3c319640c1e8b43b54
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-05 18:20:26 -0500

    feat: add support for grand finals (in hcs)

--- app/Enums/Bracket.php
+++ app/Enums/Bracket.php
@@ -5,23 +5,24 @@ namespace App\Enums;
 
 use BenSampo\Enum\Contracts\LocalizedEnum;
 use BenSampo\Enum\Enum;
-use Illuminate\Support\Str;
-use InvalidArgumentException;
 
 /**
  * @method static static WINNERS()
  * @method static static LOSERS()
+ * @method static static GRAND()
  */
 final class Bracket extends Enum implements LocalizedEnum
 {
     const WINNERS = 'winners';
     const LOSERS = 'losers';
+    const GRAND = 'grand';
 
     public static function coerce($enumKeyOrValue): ?Enum
     {
         $enumKeyOrValue = match ($enumKeyOrValue) {
             1 => self::WINNERS,
             2 => self::LOSERS,
+            3 => self::GRAND,
             default => $enumKeyOrValue
         };
 
@@ -33,6 +34,7 @@ final class Bracket extends Enum implements LocalizedEnum
         return match ((string)$this->value) {
             self::WINNERS => 1,
             self::LOSERS => 2,
+            self::GRAND => 3,
             default => null
         };
     }
