1cb53975eaefa1fe0e6afdc87ec3a1f0f5878992
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-05 18:20:46 -0500

    test: assertions for grand finals

--- tests/Feature/Pages/HcsMatchupPageTest.php
+++ tests/Feature/Pages/HcsMatchupPageTest.php
@@ -111,6 +111,9 @@ class HcsMatchupPageTest extends TestCase
             ],
             [
                 'round' => 2
+            ],
+            [
+                'round' => 3
             ]
         ];
     }
