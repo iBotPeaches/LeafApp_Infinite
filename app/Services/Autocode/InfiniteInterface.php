81c4599e6a874bb87d8ac3f79a1a6da92cd0b558
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-06 06:45:16 -0500

    feat: add mode support to customs

--- app/Services/Autocode/InfiniteInterface.php
+++ app/Services/Autocode/InfiniteInterface.php
@@ -7,13 +7,14 @@ use App\Models\Csr;
 use App\Models\Game;
 use App\Models\Player;
 use App\Models\ServiceRecord;
+use App\Services\Autocode\Enums\Mode;
 use Illuminate\Database\Eloquent\Collection;
 
 interface InfiniteInterface
 {
     public function appearance(string $gamertag): ?Player;
     public function competitive(Player $player): ?Csr;
-    public function matches(Player $player, bool $forceUpdate = false): Collection;
+    public function matches(Player $player, Mode $mode, bool $forceUpdate = false): Collection;
     public function match(string $matchUuid): ?Game;
     public function metadataMedals(): Collection;
     public function serviceRecord(Player $player): ?ServiceRecord;
