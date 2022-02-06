8ce093958cbba0714d32c12f3b78a642da56013a
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-06 07:28:56 -0500

    test: proper assertion for last game ids

--- tests/Feature/Console/PullHaloDataTest.php
+++ tests/Feature/Console/PullHaloDataTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Console;
 
+use App\Models\Game;
 use App\Models\Player;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Arr;
@@ -39,6 +40,9 @@ class PullHaloDataTest extends TestCase
         Arr::set($mockCustomMatchesResponse, 'data.0.details.playlist', null);
         Arr::set($mockCustomMatchesResponse, 'data.1.details.playlist', null);
 
+        $matchmakingGameUuid = Arr::get($mockMatchesResponse, 'data.0.id');
+        $customGameUuid = Arr::get($mockCustomMatchesResponse, 'data.0.id');
+
         Http::fakeSequence()
             ->push($mockCsrResponse, Response::HTTP_OK)
             ->push($mockMatchesResponse, Response::HTTP_OK)
@@ -55,5 +59,14 @@ class PullHaloDataTest extends TestCase
         $this
             ->artisan('app:pull-halo-data', ['player' => $player->id])
             ->assertExitCode(CommandAlias::SUCCESS);
+
+        $lastMatchmakingGame = Game::query()->firstWhere('uuid', $matchmakingGameUuid);
+        $lastCustomGame = Game::query()->firstWhere('uuid', $customGameUuid);
+
+        $this->assertDatabaseHas('players', [
+            'gamertag' => $gamertag,
+            'last_game_id_pulled' => $lastMatchmakingGame?->id,
+            'last_custom_game_id_pulled' => $lastCustomGame?->id
+        ]);
     }
 }
