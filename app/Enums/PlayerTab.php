e07e920f1f721b4ed099c5d184d8062566980c11
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-06 07:09:00 -0500

    feat: add CUSTOM support for PlayerTab

--- app/Enums/PlayerTab.php
+++ app/Enums/PlayerTab.php
@@ -9,10 +9,12 @@ use BenSampo\Enum\Enum;
  * @method static static OVERVIEW()
  * @method static static COMPETITIVE()
  * @method static static MATCHES()
+ * @method static static CUSTOM()
  */
 final class PlayerTab extends Enum implements LocalizedEnum
 {
     const OVERVIEW = 'overview';
     const COMPETITIVE = 'competitive';
     const MATCHES = 'matches';
+    const CUSTOM = 'custom';
 }
