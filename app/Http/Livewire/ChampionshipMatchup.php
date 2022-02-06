c130fa0233557b850b19cc2516a13bd6b9dec45b
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-06 15:49:54 -0500

    feat: add aggregate stats + games to hcs matchup page

--- app/Http/Livewire/ChampionshipMatchup.php
+++ app/Http/Livewire/ChampionshipMatchup.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 namespace App\Http\Livewire;
 
 use App\Models\Championship;
+use App\Models\Game;
+use App\Models\GamePlayer;
 use App\Models\Matchup;
 use Illuminate\View\View;
 use Livewire\Component;
@@ -17,9 +19,24 @@ class ChampionshipMatchup extends Component
     {
         $this->matchup->load('matchupTeams.players');
 
+        $aggregateStats = $this->matchup->games
+            ->map(function (Game $game) {
+                return $game->players->map(function (GamePlayer $gamePlayer) {
+                    return [
+                        'player' => $gamePlayer->player,
+                        'kills' => $gamePlayer->kills,
+                        'deaths' => $gamePlayer->deaths,
+                    ];
+                });
+            })
+            ->flatten(1)
+            ->keyBy('player.id');
+
         return view('livewire.championship-matchup', [
             'championship' => $this->championship,
-            'matchup' => $this->matchup
+            'matchup' => $this->matchup,
+            'games' => $this->matchup->games,
+            'aggregateStats' => $aggregateStats
         ]);
     }
 }
