262b5d67063537059688d562aedfef73714d5eeb
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-06 20:09:11 -0500

    refactor: extract account_private to message

---
+++ resources/views/partials/global/account_private.blade.php
@@ -0,0 +1,14 @@
+<article class="message is-warning">
+    <div class="message-header">
+        <p>Warning - Account Private</p>
+    </div>
+    <div class="message-body content">
+        This account is not allowing API access to information.
+        If this is your account and you want this to work. Please try:
+        <ul>
+            <li>Halo Infinite Settings</li>
+            <li>Accessibility Tab (scroll to bottom)</li>
+            <li>Share Matchmade Games</li>
+        </ul>
+    </div>
+</article>
