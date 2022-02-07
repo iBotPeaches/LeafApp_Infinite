243bb2b7dd773284f7c3622ffda72eb24a3261a4
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-07 07:14:15 -0500

    refactor: support Filter (Autocode) & Mode (Leaf) for enums

--- app/Services/Autocode/Enums/Filter.php
+++ app/Services/Autocode/Enums/Filter.php
@@ -3,10 +3,11 @@ declare(strict_types=1);
 
 namespace App\Services\Autocode\Enums;
 
+use App\Enums\Mode;
 use BenSampo\Enum\Enum;
 
 /**
- * @method static static MATCHMADE_PHP()
+ * @method static static MATCHMADE_PVP()
  * @method static static MATCHMADE_SOCIAL()
  * @method static static MATCHMADE_RANKED()
  * @method static static MATCHMADE_BOTS()
@@ -19,4 +20,16 @@ final class Filter extends Enum
     const MATCHMADE_RANKED = 'matchmade:ranked';
     const MATCHMADE_BOTS = 'matchmade:bots';
     const CUSTOM = 'custom';
+
+    public function toMode(): ?Mode
+    {
+        return match ((string)$this->value) {
+            self::MATCHMADE_PVP => Mode::MATCHMADE_PVP(),
+            self::MATCHMADE_SOCIAL => Mode::MATCHMADE_SOCIAL(),
+            self::MATCHMADE_RANKED => Mode::MATCHMADE_RANKED(),
+            self::MATCHMADE_BOTS => Mode::MATCHMADE_BOTS(),
+            self::CUSTOM => Mode::CUSTOM(),
+            default => null
+        };
+    }
 }
