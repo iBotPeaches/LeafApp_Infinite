3fbf2b7ec5c9b411e7d0ff77a97a78eb6d745f6c
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-07 07:14:02 -0500

    feat: support multiple serviceRecords
     - we do this so folks can have ranked and "everything" like they want

---
+++ database/migrations/2022_02_07_004937_add_multiple_record_support.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use App\Services\Autocode\Enums\Filter;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMultipleRecordSupport extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('service_records', function (Blueprint $table) {
+            $table->tinyInteger('mode')
+                ->default(Filter::MATCHMADE_RANKED()->toNumerical())
+                ->after('player_id');
+
+            $table->unique(['player_id', 'mode']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('service_records', function (Blueprint $table) {
+            $table->dropUnique(['player_id', 'mode']);
+            $table->dropColumn('mode');
+        });
+    }
+}
