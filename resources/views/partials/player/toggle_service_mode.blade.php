187965a094c2dfc8ea61785bf66f7cba94b947c8
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-11 06:28:09 -0500

    feat: add toggle panel for ranked vs pvp

--- resources/views/partials/player/toggle_service_mode.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="level is-mobile">
-    <div class="level-item">
-        <button class="button is-info is-fullwidth" disabled>Ranked Only</button>
-    </div>
-    <div class="level-item">
-        <button class="button is-info is-light is-fullwidth">PVP</button>
-    </div>
-</div>
