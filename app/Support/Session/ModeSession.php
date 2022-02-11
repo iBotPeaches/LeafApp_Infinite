d56580e8f9c5a12cd22b02c1795edef5c9854880
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-11 06:28:36 -0500

    feat: ModeSession class for easier session management

---
+++ app/Support/Session/ModeSession.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Support\Session;
+
+use App\Enums\Mode;
+use Illuminate\Support\Facades\Session;
+
+class ModeSession
+{
+    private static string $sessionKey = 'player-type';
+
+    public static function get(): Mode
+    {
+        $mode = Session::get(self::$sessionKey, Mode::MATCHMADE_PVP);
+
+        return Mode::coerce($mode) ?? Mode::MATCHMADE_PVP();
+    }
+
+    public static function set(int $type): void
+    {
+        Session::put(self::$sessionKey, $type);
+    }
+}
