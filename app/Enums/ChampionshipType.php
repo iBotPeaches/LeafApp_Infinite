323ccebbbf6fa5d760173664ba47f70d5ce3377d
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-12 07:00:44 -0500

    feat: new enum for type on championship

---
+++ app/Enums/ChampionshipType.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Contracts\LocalizedEnum;
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static ROUND_ROBIN()
+ * @method static static DOUBLE_ELIM()
+ * @method static static STAGE()
+ */
+final class ChampionshipType extends Enum implements LocalizedEnum
+{
+    const ROUND_ROBIN = 1;
+    const DOUBLE_ELIM = 2;
+    const STAGE = 3;
+
+    public static function coerce($enumKeyOrValue): ?Enum
+    {
+        $enumKeyOrValue = match ($enumKeyOrValue) {
+            'roundRobin' => self::ROUND_ROBIN,
+            'doubleElimination' => self::DOUBLE_ELIM,
+            'stage' => self::STAGE,
+            default => $enumKeyOrValue
+        };
+
+        return parent::coerce($enumKeyOrValue);
+    }
+
+    public function isFfa(): bool
+    {
+        return $this->is(ChampionshipType::STAGE());
+    }
+
+    public function isPoolPlay(): bool
+    {
+        return $this->is(ChampionshipType::ROUND_ROBIN());
+    }
+}
