0c08505d4b7c6fe9cc64f597cb6b56e813428e7a
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-12 07:01:15 -0500

    feat: new pool play navigation for hcs

---
+++ resources/views/partials/hcs/navigation/pool_play.blade.php
@@ -0,0 +1,32 @@
+<?php
+/** @var App\Models\Championship $championship */
+/** @var string $bracket */
+?>
+<div class="tabs is-centered">
+    <ul>
+        <li class="<?= $bracket === App\Enums\Bracket::POOL_A ? 'is-active' : null; ?>">
+            <a href="<?= route('championship', [$championship, App\Enums\Bracket::POOL_A]); ?>">
+                <span class="icon is-small"><i class="fas fa-gamepad" aria-hidden="true"></i></span>
+                <span>Pool A</span>
+            </a>
+        </li>
+        <li class="<?= $bracket === App\Enums\Bracket::POOL_B ? 'is-active' : null; ?>">
+            <a href="<?= route('championship', [$championship, App\Enums\Bracket::POOL_B]); ?>">
+                <span class="icon is-small"><i class="fas fa-gamepad" aria-hidden="true"></i></span>
+                <span>Pool B</span>
+            </a>
+        </li>
+        <li class="<?= $bracket === App\Enums\Bracket::POOL_C ? 'is-active' : null; ?>">
+            <a href="<?= route('championship', [$championship, App\Enums\Bracket::POOL_C]); ?>">
+                <span class="icon is-small"><i class="fas fa-gamepad" aria-hidden="true"></i></span>
+                <span>Pool C</span>
+            </a>
+        </li>
+        <li class="<?= $bracket === App\Enums\Bracket::POOL_D ? 'is-active' : null; ?>">
+            <a href="<?= route('championship', [$championship, App\Enums\Bracket::POOL_D]); ?>">
+                <span class="icon is-small"><i class="fas fa-gamepad" aria-hidden="true"></i></span>
+                <span>Pool D</span>
+            </a>
+        </li>
+    </ul>
+</div>
