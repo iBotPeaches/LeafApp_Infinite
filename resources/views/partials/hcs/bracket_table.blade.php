4acfa5150db13f4e5b9f81b4bd8bb9ae76745d07
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-12 06:58:57 -0500

    fix: use new isFfa() function for determining type of championship

--- resources/views/partials/hcs/bracket_table.blade.php
+++ resources/views/partials/hcs/bracket_table.blade.php
@@ -2,7 +2,7 @@
 /** @var App\Models\Matchup[]|Illuminate\Support\Collection $matchups */
 ?>
 <div class="table-container">
-    @if ($championship->is_ffa)
+    @if ($championship->type->isFfa())
         @include('partials.hcs.bracket_table.ffa')
     @else
         @include('partials.hcs.bracket_table.4v4')
