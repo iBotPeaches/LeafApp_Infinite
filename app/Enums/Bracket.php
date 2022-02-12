046ca794b978919b412a0454fa3c6cb3de011e3c
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-12 06:58:36 -0500

    feat: add pool enums (+ other) to Bracket

--- app/Enums/Bracket.php
+++ app/Enums/Bracket.php
@@ -10,19 +10,27 @@ use BenSampo\Enum\Enum;
  * @method static static WINNERS()
  * @method static static LOSERS()
  * @method static static GRAND()
+ * @method static static OTHER()
  */
 final class Bracket extends Enum implements LocalizedEnum
 {
     const WINNERS = 'winners';
     const LOSERS = 'losers';
     const GRAND = 'grand';
+    const OTHER = 'other';
+
+    const POOL_A = 'a';
+    const POOL_B = 'b';
+    const POOL_C = 'c';
+    const POOL_D = 'd';
 
     public static function coerce($enumKeyOrValue): ?Enum
     {
         $enumKeyOrValue = match ($enumKeyOrValue) {
-            1 => self::WINNERS,
-            2 => self::LOSERS,
-            3 => self::GRAND,
+            1, self::POOL_A => self::WINNERS,
+            2, self::POOL_B => self::LOSERS,
+            3, self::POOL_C => self::GRAND,
+            4, self::POOL_D => self::OTHER,
             default => $enumKeyOrValue
         };
 
@@ -35,6 +43,7 @@ final class Bracket extends Enum implements LocalizedEnum
             self::WINNERS => 1,
             self::LOSERS => 2,
             self::GRAND => 3,
+            self::OTHER => 4,
             default => null
         };
     }
