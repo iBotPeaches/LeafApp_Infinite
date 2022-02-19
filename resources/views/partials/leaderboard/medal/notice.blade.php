80aa5a77091dad8b63caa636dafd3265986ecf16
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-19 14:22:08 -0500

    fix: notice about total users

---
+++ resources/views/partials/leaderboard/medal/notice.blade.php
@@ -0,0 +1,4 @@
+<div class="notification is-light">
+    Leaf does not track as many users as <a href="https://halotracker.com">HaloTracker</a> or
+    <a href="https://halodatahive.com">HaloDataHive</a>.
+</div>
