754dd5a178bdb02ec575c05a00ca7617f8204dfa
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-21 07:12:49 -0500

    refactor: support new row split for competitive page

--- resources/views/livewire/competitive-page.blade.php
+++ resources/views/livewire/competitive-page.blade.php
@@ -1,58 +1,31 @@
 <?php
 /** @var App\Models\Csr[] $ranked */
 ?>
+@foreach ($ranked as $key => $group)
 <article class="panel is-primary">
     <p class="panel-heading">
-        Competitive Play
+        Competitive Play ({{ App\Enums\CompetitiveMode::coerce($key)->description }})
     </p>
     <div class="panel-block is-block">
         <div class="columns is-centered">
-            @foreach ($ranked as $playlist)
-                <div class="column is-flex is-flex-direction-column is-align-items-stretch">
-                    <div class="card" style="height: 100%;">
-                        <div class="card-header">
-                            <span class="card-header-title">
-                                {{ $playlist->title }}
-                                {!! $playlist->icon !!}
-                            </span>
-                        </div>
-                        <div class="card-image has-background-light">
-                            <figure class="image is-4by3">
-                                <img src="{{ $playlist->toCsrObject()->url() }}" alt="{{ $playlist->rank }}">
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <p class="title is-3">{{ $playlist->rank }}</p>
-                            <p class="subtitle is-5">
-                                @if ($playlist->matches_remaining > 0)
-                                    <i>In Placements</i>
-                                @else
-                                    CSR: {{ number_format($playlist->csr) }}
-                                @endif
-                            </p>
-
-                            <div class="content">
-                                @if (! $playlist->isOnyx())
-                                    <progress
-                                        class="progress {{ $playlist->getRankPercentColor() }}"
-                                        value="{{ $playlist->current_xp_for_level }}"
-                                        max="{{ $playlist->next_xp_for_level }}"
-                                    >%{{ number_format($playlist->next_rank_percent, 2) }}</progress>
-                                    <br>
-                                    @if ($playlist->hasNextRank())
-                                        <span>Up Next: {{ $playlist->next_rank }}</span>
-                                    @else
-                                        <span>
-                                            {{ $playlist->matches_remaining }}
-                                            {{ Str::plural('match', $playlist->matches_remaining) }} remaining.
-                                        </span>
-                                    @endif
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            @foreach ($group as $playlist)
+                @include('partials.player.csr-card-row')
             @endforeach
         </div>
     </div>
 </article>
+@endforeach
+@if ($allTime)
+    <article class="panel is-info">
+        <p class="panel-heading">
+            Competitive Play (All Time)
+        </p>
+        <div class="panel-block is-block">
+            <div class="columns is-centered">
+                @foreach ($allTime as $playlist)
+                    @include('partials.player.csr-card-row')
+                @endforeach
+            </div>
+        </div>
+    </article>
+@endif
