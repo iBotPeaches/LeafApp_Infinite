d612ac8b6e672c5db29a8d3fd65213e63cf8722e
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-21 07:13:27 -0500

    fix: coerce mode properly on csr

--- app/Enums/CompetitiveMode.php
+++ app/Enums/CompetitiveMode.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 
 use BenSampo\Enum\Contracts\LocalizedEnum;
 use BenSampo\Enum\Enum;
+use Illuminate\Support\Str;
 
 /**
  * @method static static CURRENT()
@@ -16,4 +17,13 @@ final class CompetitiveMode extends Enum implements LocalizedEnum
     const CURRENT = 1;
     const SEASON = 2;
     const ALL_TIME = 3;
+
+    public static function coerce($enumKeyOrValue): ?Enum
+    {
+        $enumKeyOrValue = is_numeric($enumKeyOrValue)
+            ? $enumKeyOrValue
+            : Str::upper(Str::replace('-', '_', $enumKeyOrValue));
+
+        return parent::coerce($enumKeyOrValue);
+    }
 }
