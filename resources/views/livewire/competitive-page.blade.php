9b90de8a607fe6635cefa155784c8d85333b5d96
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-02-22 06:50:52 -0500

    feat: ui changes for competitive alltime vs season

--- resources/views/livewire/competitive-page.blade.php
+++ resources/views/livewire/competitive-page.blade.php
@@ -1,31 +1,44 @@
 <?php
 /** @var App\Models\Csr[] $ranked */
 ?>
-@foreach ($ranked as $key => $group)
-<article class="panel is-primary">
-    <p class="panel-heading">
-        Competitive Play ({{ App\Enums\CompetitiveMode::coerce($key)->description }})
-    </p>
-    <div class="panel-block is-block">
-        <div class="columns is-centered">
-            @foreach ($group as $playlist)
-                @include('partials.player.csr-card-row')
-            @endforeach
-        </div>
-    </div>
-</article>
-@endforeach
-@if ($allTime)
-    <article class="panel is-info">
+@if ($current)
+    <article class="panel is-primary">
         <p class="panel-heading">
-            Competitive Play (All Time)
+            Competitive Play (Current)
         </p>
         <div class="panel-block is-block">
             <div class="columns is-centered">
-                @foreach ($allTime as $playlist)
+                @foreach ($current as $playlist)
                     @include('partials.player.csr-card-row')
                 @endforeach
             </div>
         </div>
     </article>
+@endif
+@if ($season->isNotEmpty() && $allTime->isNotEmpty())
+    <div class="divider">Records</div>
+    <div class="columns">
+        <div class="column">
+            <h5 class="title is-5">
+                Season High
+                <span class="subtitle is-6 is-pulled-right">
+                    CSR
+                </span>
+            </h5>
+            @foreach ($season as $playlist)
+                @include('partials.player.csr-minimal-card-row')
+            @endforeach
+        </div>
+        <div class="column">
+            <h5 class="title is-5">
+                All Time
+                <span class="subtitle is-6 is-pulled-right">
+                    CSR
+                </span>
+            </h5>
+            @foreach ($allTime as $playlist)
+                @include('partials.player.csr-minimal-card-row')
+            @endforeach
+        </div>
+    </div>
 @endif
