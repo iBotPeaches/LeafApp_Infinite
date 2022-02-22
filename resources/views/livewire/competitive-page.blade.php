647312def89248b65dfa70120d64b645a46f538f
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2022-02-22 07:25:53 -0500

    fix: don't show historic csr if no data (#266)
    * fix: don't show historic csr if no data
    
    * refactor: root node for livewire refresh
    
    * refactor: properly check for placements done
    
    * test: adding more assertions for csr season/allTime

--- resources/views/livewire/competitive-page.blade.php
+++ resources/views/livewire/competitive-page.blade.php
@@ -1,44 +1,50 @@
 <?php
 /** @var App\Models\Csr[] $ranked */
 ?>
-@if ($current)
-    <article class="panel is-primary">
-        <p class="panel-heading">
-            Competitive Play (Current)
-        </p>
-        <div class="panel-block is-block">
-            <div class="columns is-centered">
-                @foreach ($current as $playlist)
-                    @include('partials.player.csr-card-row')
+<div>
+    @if ($current)
+        <article class="panel is-primary">
+            <p class="panel-heading">
+                Competitive Play (Current)
+            </p>
+            <div class="panel-block is-block">
+                <div class="columns is-centered">
+                    @foreach ($current as $playlist)
+                        @include('partials.player.csr-card-row')
+                    @endforeach
+                </div>
+            </div>
+        </article>
+    @endif
+    @if ($season->isNotEmpty() && $allTime->isNotEmpty())
+        <div class="divider">Records</div>
+        <div class="columns">
+            <div class="column">
+                <h5 class="title is-5">
+                    Season High
+                    <span class="subtitle is-6 is-pulled-right">
+                    CSR
+                </span>
+                </h5>
+                @foreach ($season as $playlist)
+                    @if ($playlist->hasPlacementsDone())
+                        @include('partials.player.csr-minimal-card-row')
+                    @endif
+                @endforeach
+            </div>
+            <div class="column">
+                <h5 class="title is-5">
+                    All Time
+                    <span class="subtitle is-6 is-pulled-right">
+                    CSR
+                </span>
+                </h5>
+                @foreach ($allTime as $playlist)
+                    @if ($playlist->hasPlacementsDone())
+                        @include('partials.player.csr-minimal-card-row')
+                    @endif
                 @endforeach
             </div>
         </div>
-    </article>
-@endif
-@if ($season->isNotEmpty() && $allTime->isNotEmpty())
-    <div class="divider">Records</div>
-    <div class="columns">
-        <div class="column">
-            <h5 class="title is-5">
-                Season High
-                <span class="subtitle is-6 is-pulled-right">
-                    CSR
-                </span>
-            </h5>
-            @foreach ($season as $playlist)
-                @include('partials.player.csr-minimal-card-row')
-            @endforeach
-        </div>
-        <div class="column">
-            <h5 class="title is-5">
-                All Time
-                <span class="subtitle is-6 is-pulled-right">
-                    CSR
-                </span>
-            </h5>
-            @foreach ($allTime as $playlist)
-                @include('partials.player.csr-minimal-card-row')
-            @endforeach
-        </div>
-    </div>
-@endif
+    @endif
+</div>
