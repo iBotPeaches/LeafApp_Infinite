e4e21fe27e402c6004ee6c3e4f4cd0eae1a5ea73
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-02 07:06:26 -0500

    feat: new command/job to pull historic csr data

---
+++ app/Console/Commands/PullHistoricCompetitive.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Console\Commands;
+
+use App\Jobs\PullCompetitiveHistory;
+use App\Models\Player;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class PullHistoricCompetitive extends Command
+{
+    protected $signature = 'app:pull-historic-competitive';
+    protected $description = 'Slowly chunks record(s) for CSR update.';
+
+    public function handle(): int
+    {
+        $csrKey = config('services.autocode.competitive.season')
+            . config('services.autocode.competitive.version');
+
+        Player::query()
+            ->where('last_csr_key', '<>', $csrKey)
+            ->orWhereNull('last_csr_key')
+            ->orderByDesc('id')
+            ->limit(10)
+            ->cursor()
+            ->each(function (Player $player) use ($csrKey) {
+                PullCompetitiveHistory::dispatch($player);
+
+                $player->last_csr_key = $csrKey;
+                $player->saveOrFail();
+            });
+
+        return CommandAlias::SUCCESS;
+    }
+}
