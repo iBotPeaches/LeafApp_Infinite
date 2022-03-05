3d3785f8ed861bffb1732c8f1ec65c5d68323ba0
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-05 07:27:41 -0500

    feat: add users table

---
+++ database/migrations/2022_03_05_121859_add_users_table.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use App\Models\Player;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Player::class)->nullable(true)->constrained()->nullOnDelete();
+            $table->string('google_id', 32);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
