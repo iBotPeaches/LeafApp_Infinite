5f18008e4c033341991ab31b06c06e5539cb00c5
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-05 11:25:51 -0500

    feat: add scrim table

---
+++ database/migrations/2022_03_05_140233_add_scrim_tables.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+use App\Models\Game;
+use App\Models\Scrim;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('scrims', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+
+        Schema::create('game_scrim', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Scrim::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Game::class)->constrained()->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('scrim_game');
+        Schema::dropIfExists('scrims');
+    }
+};
