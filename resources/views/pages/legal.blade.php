e0de4f415f6c4a0e27d34c10f3003ddcb2443111
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-05 06:54:40 -0500

    feat: legal page for adding google login

---
+++ resources/views/pages/legal.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', 'Leaf - Legal')
+
+@section('content')
+    <article class="message is-link">
+        <div class="message-header">
+            <p>Legal</p>
+        </div>
+        <div class="message-body">
+            This application uses an optional Google login to prevent malicious edits to scrim information. The only
+            information captured is the internal googleId. Nothing else - I do not want any other information.
+        </div>
+    </article>
+@endsection
