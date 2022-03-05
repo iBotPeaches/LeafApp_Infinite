1e75565e1652b1d660e85ee2de28cf6009c0bf11
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-05 11:28:36 -0500

    feat: add overview tab

---
+++ app/Enums/ScrimTab.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Contracts\LocalizedEnum;
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OVERVIEW()
+ */
+final class ScrimTab extends Enum implements LocalizedEnum
+{
+    const OVERVIEW = 'overview';
+}
