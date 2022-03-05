5037e1762446118e52069ecba0402de038b70443
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-05 07:27:33 -0500

    build: add env support for google oauth

--- config/services.php
+++ config/services.php
@@ -54,4 +54,10 @@ return [
         ]
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT'),
+    ],
+
 ];
