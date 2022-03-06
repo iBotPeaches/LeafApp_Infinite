c1350bc6f668f81ca62cd7182642188bbc96c066
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-06 18:46:09 -0500

    fix: add matches/players tabs to scrim

--- resources/views/partials/scrim/navigation.blade.php
+++ resources/views/partials/scrim/navigation.blade.php
@@ -5,9 +5,21 @@
 <div class="tabs is-fullwidth is-centered is-toggle">
     <ul>
         <li class="<?= $type === App\Enums\ScrimTab::OVERVIEW ? 'is-active' : null; ?>">
-            <a href="<?= route('player', [$scrim, App\Enums\ScrimTab::OVERVIEW]); ?>">
+            <a href="<?= route('scrim', [$scrim, App\Enums\ScrimTab::OVERVIEW]); ?>">
                 <span class="icon"><i class="fa fa-briefcase"></i></span>
                 <span>Overview</span>
+            </a>
+        </li>
+        <li class="<?= $type === App\Enums\ScrimTab::MATCHES ? 'is-active' : null; ?>">
+            <a href="<?= route('scrim', [$scrim, App\Enums\ScrimTab::MATCHES]); ?>">
+                <span class="icon"><i class="fa fa-history"></i></span>
+                <span>Matches</span>
+            </a>
+        </li>
+        <li class="<?= $type === App\Enums\ScrimTab::PLAYERS ? 'is-active' : null; ?>">
+            <a href="<?= route('scrim', [$scrim, App\Enums\ScrimTab::PLAYERS]); ?>">
+                <span class="icon"><i class="fa fa-users"></i></span>
+                <span>Players</span>
             </a>
         </li>
     </ul>
