7553d3e396eef5d67ce1a02901624d9da0c4d14d
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-06 18:48:20 -0500

    fix: remove improper rendering overview code

--- app/Http/Livewire/ScrimOverview.php
+++ app/Http/Livewire/ScrimOverview.php
@@ -3,8 +3,6 @@ declare(strict_types = 1);
 
 namespace App\Http\Livewire;
 
-use App\Enums\Outcome;
-use App\Models\Game;
 use App\Models\Scrim;
 use Illuminate\View\View;
 use Livewire\Component;
@@ -15,27 +13,6 @@ class ScrimOverview extends Component
 
     public function render(): View
     {
-        /** @var Game $game */
-        $this->scrim->load('games.teams.players.player');
-        $game = $this->scrim->games->first();
-
-        $team1 = $game->findTeamFromInternalId(0);
-        $team1['points'] = 0;
-        $team2 = $game->findTeamFromInternalId(1);
-        $team2['points'] = 0;
-
-        $this->scrim->games->each(function (Game $game) use ($team1, $team2) {
-            if ($game->findTeamFromInternalId($team1->internal_team_id)->outcome->is(Outcome::WIN())) {
-                $team1['points'] += 1;
-            }
-            if ($game->findTeamFromInternalId($team2->internal_team_id)->outcome->is(Outcome::WIN())) {
-                $team2['points'] += 1;
-            }
-        });
-
-        return view('livewire.scrim-overview', [
-            'team1' => $team1,
-            'team2' => $team2,
-        ]);
+        return view('livewire.scrim-overview');
     }
 }
