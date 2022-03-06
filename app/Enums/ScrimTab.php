dd16019141df641c50fe96c4e4b732a46284565c
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-06 18:44:56 -0500

    fix: add new matches/players tab for scrims

--- app/Enums/ScrimTab.php
+++ app/Enums/ScrimTab.php
@@ -7,8 +7,12 @@ use BenSampo\Enum\Enum;
 
 /**
  * @method static static OVERVIEW()
+ * @method static static MATCHES()
+ * @method static static PLAYERS()
  */
 final class ScrimTab extends Enum implements LocalizedEnum
 {
     const OVERVIEW = 'overview';
+    const MATCHES = 'matches';
+    const PLAYERS = 'players';
 }
