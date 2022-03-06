5246944bb39f08e56ad2ff9707977dc35fac2a8f
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-06 18:48:08 -0500

    fix: add under construction page to scrim overview

--- resources/views/livewire/scrim-overview.blade.php
+++ resources/views/livewire/scrim-overview.blade.php
@@ -1,14 +1,1 @@
-<div class="columns">
-    <div class="column">
-        @include('partials.scrim.team_breakdown', [
-            'color' => 'danger',
-            'team' => $team1
-        ])
-    </div>
-    <div class="column">
-        @include('partials.scrim.team_breakdown', [
-            'color' => 'info',
-            'team' => $team2
-        ])
-    </div>
-</div>
+@include('partials.global.under_construction')
