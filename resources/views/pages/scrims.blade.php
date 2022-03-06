e86ad6f94143cb74ddd68d2dc7b939b870b2833f
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-05 21:02:19 -0500

    feat: scrims table

---
+++ resources/views/pages/scrims.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Leaf Scrims')
+@section('description', 'Leaf Scrims')
+
+@section('content')
+    <div class="columns">
+        <div class="column">
+            <livewire:scrims-table></livewire:scrims-table>
+        </div>
+    </div>
+@endsection
