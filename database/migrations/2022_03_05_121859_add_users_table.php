1023a3220f2aa2d81b13a86bba926db917837605
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-06 19:07:34 -0500

    fix: make remember_me nullable on users

--- database/migrations/2022_03_05_121859_add_users_table.php
+++ database/migrations/2022_03_05_121859_add_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Player::class)->nullable(true)->constrained()->nullOnDelete();
-            $table->string('google_id', 32);
+            $table->string('google_id', 32)->nullable(true);
             $table->timestamps();
         });
     }
