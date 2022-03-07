d3a095da9929ee0022aead3b27716d9ead2607ce
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-06 19:04:24 -0500

    feat: support for scrim players

---
+++ app/Http/Livewire/ScrimPlayers.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Http\Livewire;
+
+use App\Models\Game;
+use App\Models\GamePlayer;
+use App\Models\Scrim;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class ScrimPlayers extends Component
+{
+    public Scrim $scrim;
+
+    public array $mergedStats;
+
+    public function render(): View
+    {
+        $this->scrim->load([
+            'games.players.player'
+        ]);
+
+        $this->scrim->games->each(function (Game $game) {
+            $game->players->each(function (GamePlayer $gamePlayer) {
+                $playerId = $gamePlayer->player_id;
+
+                if (! isset($this->mergedStats[$playerId])) {
+                    $this->mergedStats[$playerId] = $gamePlayer;
+                    $this->mergedStats[$playerId]['gameCount'] = 1;
+                } else {
+                    $this->mergedStats[$playerId]['gameCount'] += 1;
+
+                    $this->mergedStats[$playerId]['kills'] += $gamePlayer->kills;
+                    $this->mergedStats[$playerId]['deaths'] += $gamePlayer->deaths;
+                    $this->mergedStats[$playerId]['assists'] += $gamePlayer->assists;
+                    $this->mergedStats[$playerId]['kd'] += $gamePlayer->kd;
+                    $this->mergedStats[$playerId]['kda'] += $gamePlayer->kda;
+                    $this->mergedStats[$playerId]['accuracy'] += $gamePlayer->accuracy;
+                    $this->mergedStats[$playerId]['score'] += $gamePlayer->score;
+                    $this->mergedStats[$playerId]['rank'] += $gamePlayer->rank;
+                }
+            });
+        });
+
+        foreach ($this->mergedStats as &$mergedStat) {
+            $mergedStat['kd'] = $mergedStat['kd'] / $mergedStat['gameCount'];
+            $mergedStat['kda'] = $mergedStat['kda'] / $mergedStat['gameCount'];
+            $mergedStat['accuracy'] = $mergedStat['accuracy'] / $mergedStat['gameCount'];
+            $mergedStat['score'] = $mergedStat['score'] / $mergedStat['gameCount'];
+            $mergedStat['rank'] = $mergedStat['rank'] / $mergedStat['gameCount'];
+        }
+
+        usort($this->mergedStats, function (GamePlayer $a, GamePlayer $b) {
+            return $a['rank'] <=> $b['rank'];
+        });
+
+        return view('livewire.scrim-players', [
+            'gamePlayers' => $this->mergedStats
+        ]);
+    }
+}
---
+++ resources/views/livewire/scrim-players.blade.php
@@ -0,0 +1,60 @@
+<div class="table-container">
+    <table class="table is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+        <tr>
+            <th>Gamertag</th>
+            <th><abbr title="Kills">K</abbr></th>
+            <th><abbr title="Deaths">D</abbr></th>
+            <th><abbr title="Assists">A</abbr></th>
+            <th><abbr title="Kills / Deaths">KD</abbr></th>
+            <th><abbr title="Kills + Assists / Deaths">KDA</abbr></th>
+            <th><abbr title="Shots Hit / Shots Taken">Accuracy</abbr></th>
+            <th><abbr title="Ordered by Points">Avg Score</abbr></th>
+            <th>Avg Rank</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($gamePlayers as $gamePlayer)
+            <tr>
+                <td>
+                    <article class="media">
+                        <figure class="media-left">
+                            <p class="image is-32x32">
+                                @include('partials.game.team_emblem_url')
+                            </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content" style="white-space: nowrap">
+                                <a href="{{ route('player', [$gamePlayer->player]) }}">
+                                    {{ $gamePlayer->player->gamertag }}
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                </td>
+                <td>{{ $gamePlayer->kills }}</td>
+                <td>{{ $gamePlayer->deaths }}</td>
+                <td>{{ $gamePlayer->assists }}</td>
+                <td class="{{ $gamePlayer->getKdColor() }}">
+                    {{ $gamePlayer->kd }}
+                </td>
+                <td class="{{ $gamePlayer->getKdaColor() }}">
+                    {{ $gamePlayer->kda }}
+                </td>
+                <td class="has-background-{{ $gamePlayer->accuracy_color }}-light">
+                    {{ number_format($gamePlayer->accuracy, 2) }}%
+                </td>
+                <td>{{ number_format($gamePlayer->score) }}</td>
+                <td>
+                    {{ $gamePlayer->rank }}
+                    @if (!$gamePlayer->was_at_end)
+                        <span class="tag is-danger">
+                                <abbr title="Did Not Finish (Quit/Crashed)">DNF</abbr>
+                            </span>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
