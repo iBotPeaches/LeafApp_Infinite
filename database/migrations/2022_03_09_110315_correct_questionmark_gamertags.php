96993ca6d6592d4f4fdf53cfa773bd0138c2b417
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2022-03-09 06:13:46 -0500

    Support unresolved gamertags during match pulls (#287)
    * fix: migration to clear up ??? gamers
    
    * fix: skip not processed gamers
    
    * test: assertion skipping players if "???"
    
    * fix: properly save game (of affected records)

---
+++ database/migrations/2022_03_09_110315_correct_questionmark_gamertags.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use App\Models\Game;
+use App\Models\Player;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        /** @var Player $gamer */
+        $gamer = Player::query()
+            ->where('gamertag', '???')
+            ->first();
+
+        if ($gamer) {
+            $gamer->games->each(function (Game $game) {
+                $game->was_pulled = false;
+                $game->saveOrFail();
+            });
+
+            $gamer->deleteOrFail();
+        }
+    }
+
+    public function down(): void
+    {
+        //
+    }
+};
