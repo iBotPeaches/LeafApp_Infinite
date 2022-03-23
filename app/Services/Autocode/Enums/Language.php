9f28b9289e1172e595cec1b82eb01d04c444cfef
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-22 20:45:04 -0400

    feat: introduce Language enum

---
+++ app/Services/Autocode/Enums/Language.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services\Autocode\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static US()
+ */
+final class Language extends Enum
+{
+    const US = 'en-us';
+}
