e31655469c8ff2fc0a8d5c44a76526c2dd8aeedd
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-23 07:29:58 -0400

    fix: remove PullHistoricCompetitive job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Console\Commands\PullHistoricCompetitive;
 use App\Console\Commands\PullMetadata;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -22,10 +21,6 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(PullHistoricCompetitive::class)
-            ->withoutOverlapping()
-            ->everyThirtyMinutes();
-
         $schedule->command(PullMetadata::class)
             ->withoutOverlapping()
             ->twiceDaily();
