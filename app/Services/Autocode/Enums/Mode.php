ca371730448d7e23c961961af35847a1292666ef
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-22 20:45:47 -0400

    feat: add support for lan game mode

--- app/Services/Autocode/Enums/Mode.php
+++ app/Services/Autocode/Enums/Mode.php
@@ -8,9 +8,20 @@ use BenSampo\Enum\Enum;
 /**
  * @method static static MATCHMADE()
  * @method static static CUSTOM()
+ * @method static static LAN()
  */
 final class Mode extends Enum
 {
-    const MATCHMADE = 'matchmade';
+    const MATCHMADE = 'matchmaking';
     const CUSTOM = 'custom';
+    const LAN = 'local';
+
+    public function getLastGameIdVariable(): string
+    {
+        return match ($this->value) {
+            self::MATCHMADE => 'last_game_id_pulled',
+            self::CUSTOM => 'last_custom_game_id_pulled',
+            self::LAN => 'last_lan_game_id_pulled'
+        };
+    }
 }
