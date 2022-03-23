6ea8922e1f7dc8afade27990897bb7e3152a5f14
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-22 20:55:45 -0400

    refactor: swap all mocks to 1.0.0 beta

--- tests/Mocks/ServiceRecord/MockServiceRecordService.php
+++ tests/Mocks/ServiceRecord/MockServiceRecordService.php
@@ -15,74 +15,123 @@ class MockServiceRecordService extends BaseMock
     {
         return [
             'data' => [
-                'core' => [
-                    'summary' => [
-                        'kills' => $this->faker->numberBetween(1, 25),
-                        'deaths' => $this->faker->numberBetween(1, 25),
-                        'assists' => $this->faker->numberBetween(1, 25),
-                        'betrayals' => $this->faker->numberBetween(0, 5),
-                        'suicides' => $this->faker->numberBetween(0, 5),
+                'records' => [
+                    'pvp' => $this->recordStats(),
+                    'ranked' => $this->recordStats(),
+                    'social' => $this->recordStats(),
+                ],
+                'privacy' => [
+                    'public' => true,
+                ],
+            ],
+            'additional' => [
+                'polling' => [
+                    'synced_at' => now()->toIso8601ZuluString(),
+                ],
+                'parameters' => [
+                    'gamertag' => $gamertag,
+                    'filter' => Filter::MATCHMADE_RANKED
+                ]
+            ]
+        ];
+    }
+
+    public function recordStats(): array
+    {
+        return [
+            'core' => [
+                'summary' => [
+                    'kills' => $this->faker->numberBetween(1, 25),
+                    'deaths' => $this->faker->numberBetween(1, 25),
+                    'assists' => $this->faker->numberBetween(1, 25),
+                    'betrayals' => $this->faker->numberBetween(0, 5),
+                    'suicides' => $this->faker->numberBetween(0, 5),
+                    'vehicles' => [
+                        'destroys' => $this->faker->numberBetween(0, 2),
+                        'hijacks' => $this->faker->numberBetween(0, 2),
+                    ],
+                    'medals' => $this->faker->numberBetween(1, 25)
+                ],
+                'damage' => [
+                    'taken' => $this->faker->numerify('######'),
+                    'dealt' => $this->faker->numerify('######'),
+                ],
+                'shots' => [
+                    'fired' => $this->faker->numerify(),
+                    'landed' => $this->faker->numerify(),
+                    'missed' => $this->faker->numerify(),
+                    'accuracy' => $this->faker->randomFloat(2, 0, 100)
+                ],
+                'rounds' => [
+                    'won' => $this->faker->numerify(),
+                    'lost' => $this->faker->numerify(),
+                    'tied' => $this->faker->numerify()
+                ],
+                'breakdowns' => [
+                    'kills' => [
+                        'melee' => $this->faker->numberBetween(0, 5),
+                        'grenades' => $this->faker->numberBetween(0, 5),
+                        'headshots' => $this->faker->numberBetween(0, 5),
+                        'power_weapons' => $this->faker->numberBetween(0, 5),
+                        'assassinations' => $this->faker->numberBetween(0, 5),
                         'vehicles' => [
-                            'destroys' => $this->faker->numberBetween(0, 2),
-                            'hijacks' => $this->faker->numberBetween(0, 2),
+                            'splatters' => $this->faker->numberBetween(0, 5),
                         ],
-                        'medals' => $this->faker->numberBetween(1, 25)
+                        'miscellaneous' => [
+                            'repulsor' => $this->faker->numberBetween(0, 5),
+                            'fusion_coils' => $this->faker->numberBetween(0, 5),
+                        ]
                     ],
-                    'damage' => [
-                        'taken' => $this->faker->numerify('######'),
-                        'dealt' => $this->faker->numerify('######'),
-                        'average' => $this->faker->numerify('####')
+                    'assists' => [
+                        'emp' => $this->faker->numberBetween(0, 5),
+                        'driver' => $this->faker->numberBetween(0, 5),
+                        'callouts' => $this->faker->numberBetween(0, 5)
                     ],
-                    'shots' => [
-                        'fired' => $this->faker->numerify(),
-                        'landed' => $this->faker->numerify(),
-                        'missed' => $this->faker->numerify(),
-                        'accuracy' => $this->faker->randomFloat(2, 0, 100)
-                    ],
-                    'breakdowns' => [
-                        'kills' => [
-                            'melee' => $this->faker->numberBetween(0, 5),
-                            'grenades' => $this->faker->numberBetween(0, 5),
-                            'headshots' => $this->faker->numberBetween(0, 5),
-                            'power_weapons' => $this->faker->numberBetween(0, 5),
-                        ],
-                        'assists' => [
-                            'emp' => $this->faker->numberBetween(0, 5),
-                            'driver' => $this->faker->numberBetween(0, 5),
-                            'callouts' => $this->faker->numberBetween(0, 5)
-                        ],
-                        'matches' => [
-                            'wins' => $this->faker->numberBetween(0, 25),
-                            'losses' => $this->faker->numberBetween(0, 25),
-                            'left' => $this->faker->numberBetween(0, 25),
-                            'draws' => $this->faker->numberBetween(0, 25),
-                        ],
-                        'medals' => [
+                    'vehicles' => [
+                        'destroys' => [
                             [
-                                'id' => 3233952928,
-                                'count' => $this->faker->numberBetween(0, 25),
-                            ],
-                            [
-                                'id' => 1169571763,
-                                'count' => $this->faker->numberBetween(0, 25),
+                                'value' => 'warthog',
+                                'count' => $this->faker->numberBetween(0, 5)
                             ]
                         ],
+                        'hijacks' => [
+                            [
+                                'value' => 'warthog',
+                                'count' => $this->faker->numberBetween(0, 5)
+                            ]
+                        ]
                     ],
-                    'kda' => $this->faker->randomFloat(2, 0, 10),
-                    'kdr' => $this->faker->randomFloat(2, 0, 10),
-                    'total_score' => $this->faker->numerify('######'),
+                    'medals' => [
+                        [
+                            'id' => 3233952928,
+                            'count' => $this->faker->numberBetween(0, 25),
+                        ],
+                        [
+                            'id' => 1169571763,
+                            'count' => $this->faker->numberBetween(0, 25),
+                        ]
+                    ],
                 ],
-                'matches_played' => $this->faker->numerify(),
+                'kda' => $this->faker->randomFloat(2, 0, 10),
+                'kdr' => $this->faker->randomFloat(2, 0, 10),
+                'scores' => [
+                    'personal' => $this->faker->numerify('######'),
+                    'points' => $this->faker->numerify('######'),
+                ],
+                'matches' => [
+                    'outcomes' => [
+                        'wins' => $this->faker->numberBetween(0, 25),
+                        'draws' => $this->faker->numberBetween(0, 25),
+                        'losses' => $this->faker->numberBetween(0, 25),
+                        'left' => $this->faker->numberBetween(0, 25),
+                    ],
+                    'total' => $this->faker->numberBetween(0, 25)
+                ],
                 'time_played' => [
                     'seconds' => $this->faker->numerify('######'),
                     'human' => ''
-                ],
-                'win_rate' => $this->faker->numerify('##.##'),
+                ]
             ],
-            'additional' => [
-                'gamertag' => $gamertag,
-                'filter' => Filter::MATCHMADE_RANKED
-            ]
         ];
     }
 }
