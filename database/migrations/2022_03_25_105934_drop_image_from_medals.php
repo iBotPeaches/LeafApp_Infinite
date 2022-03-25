d4429325a451c58fc49d6d80605e6b5990a8ee76
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-03-25 07:00:11 -0400

    feat: remove image from medals

---
+++ database/migrations/2022_03_25_105934_drop_image_from_medals.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('medals', function (Blueprint $table) {
+            $table->dropColumn('thumbnail_url');
+        });
+    }
+
+    public function down(): void
+    {
+        throw new Exception('Reversing this migration is not supported.');
+    }
+};
