ec89bb1bac872a6a16e749c5707489982c81b208
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-04-15 07:24:46 -0400

    feat: add mmr to gameplayers (nullable)

---
+++ database/migrations/2022_04_15_111651_add_mmr_to_gameplayers.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('game_players', function (Blueprint $table) {
+            $table->decimal('mmr', 7, 3)->nullable(true)->after('score');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('game_players', function (Blueprint $table) {
+            $table->dropColumn('mmr');
+        });
+    }
+};
