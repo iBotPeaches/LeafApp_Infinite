7b97915db409977ddc4b6be0cc42faa7f1e22e8a
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-04-15 06:44:07 -0400

    refactor: use notification for design on breadcrumbs

--- resources/views/partials/leaderboard/medal/breadcrumbs.blade.php
+++ resources/views/partials/leaderboard/medal/breadcrumbs.blade.php
@@ -1,6 +1,8 @@
-<nav class="breadcrumb" aria-label="breadcrumbs">
-    <ul>
-        <li><a href="{{ route('medalLeaderboards') }}">Medal Leaderboards</a></li>
-        <li class="is-active"><a href="#">{{ $medal->name }}</a></li>
-    </ul>
-</nav>
+<div class="notification mb-1">
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+            <li><a href="{{ route('medalLeaderboards') }}">Medals</a></li>
+            <li class="is-active"><a href="#" aria-current="page" style="text-decoration: none;">{{ $medal->name }}</a></li>
+        </ul>
+    </nav>
+</div>
