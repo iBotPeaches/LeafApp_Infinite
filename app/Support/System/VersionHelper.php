9e0eb3c3e1ac0110a2ec6985a85bf212fd6b1b64
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2022-04-25 06:26:31 -0400

    build: deploy version file (during dep deploy) (#334)
    * build: deploy version file (during dep deploy)
    
    * fix: add version to footer
    
    * test: assertion for valid version string

---
+++ app/Support/System/VersionHelper.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Support\System;
+
+use Illuminate\Support\Facades\File;
+
+class VersionHelper
+{
+    public static function getVersionString(): ?string
+    {
+        $versionFile = base_path('VERSION');
+
+        if (File::exists($versionFile)) {
+            return File::get($versionFile);
+        }
+
+        return null;
+    }
+}
