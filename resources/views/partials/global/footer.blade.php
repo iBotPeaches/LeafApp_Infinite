f565d89f65bb938cc3094112680dcbd4da664553
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-04-25 06:23:23 -0400

    fix: add version to footer

--- resources/views/partials/global/footer.blade.php
+++ resources/views/partials/global/footer.blade.php
@@ -1,8 +1,22 @@
+<?php
+$version = App\Support\System\VersionHelper::getVersionString();
+?>
 <footer class="footer mt-auto">
     <div class="content has-text-centered">
         <p>
-            <strong>Leaf</strong> by <a href="https://twitter.com/iBotPeaches">iBotPeaches</a>. The source code is available at
-            <a href="https://github.com/iBotPeaches/LeafApp_Infinite">GitHub</a>. API Data from <a href="https://autocode.com/lib/halo/">HaloDotAPI (Autocode)</a>
+            <strong>Leaf</strong>
+            @if ($version)
+                <a
+                    href="https://github.com/iBotPeaches/LeafApp_Infinite/releases/tag/{{ $version }}"
+                    target="_blank"
+                >
+                    {{ $version }}
+                </a>
+            @endif
+            by <a href="https://twitter.com/iBotPeaches">iBotPeaches</a>. The source code is at
+            <a href="https://github.com/iBotPeaches/LeafApp_Infinite">GitHub</a>.
+
+            API Data from <a href="https://autocode.com/lib/halo/">HaloDotAPI (Autocode)</a>
         </p>
     </div>
 </footer>
