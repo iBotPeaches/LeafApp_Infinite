843d20caa6a18c2e35ec05ea041cbe57db7ed49b
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-05-02 19:31:17 -0400

    feat: SeasonSession for pivot

--- app/Http/Livewire/PlayerTogglePanel.php
+++ app/Http/Livewire/PlayerTogglePanel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Http\Livewire;
 
 use App\Support\Session\ModeSession;
+use App\Support\Session\SeasonSession;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -15,16 +16,19 @@ class PlayerTogglePanel extends Component
     public function onChange(): void
     {
         ModeSession::set($this->playerType);
+        $this->emitToComponents();
+    }
 
-        $this->emitTo(OverviewPage::class, '$refresh');
-        $this->emitTo(MedalsPage::class, '$refresh');
-        $this->emitTo(MedalsLeaderboard::class, '$refresh');
+    public function onSeasonChange(): void
+    {
+        SeasonSession::set($this->season);
+        $this->emitToComponents();
     }
 
     public function mount(): void
     {
         $this->playerType = (int)ModeSession::get()->value;
-        $this->season = (int)config('services.autocode.competitive.season');
+        $this->season = SeasonSession::get();
     }
 
     public function render(): View
@@ -33,5 +37,12 @@ class PlayerTogglePanel extends Component
             'playerType' => $this->playerType,
             'season' => $this->season
         ]);
+    }
+
+    private function emitToComponents(): void
+    {
+        $this->emitTo(OverviewPage::class, '$refresh');
+        $this->emitTo(MedalsPage::class, '$refresh');
+        $this->emitTo(MedalsLeaderboard::class, '$refresh');
     }
 }
