c0be715f20436f96b9b34e482c6f66644b1ed3ec
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-05-03 06:00:24 -0400

    test: assertion for running xuid job in isolation

---
+++ tests/Feature/Jobs/PullXuidTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Feature\Jobs;
+
+use App\Jobs\PullXuid;
+use App\Models\Player;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Mocks\Xuid\MockXuidService;
+use Tests\TestCase;
+
+class PullXuidTest extends TestCase
+{
+    use WithFaker;
+
+    public function testDeletingFailedXuidJob(): void
+    {
+        // Arrange
+        $mockXuidResponse = (new MockXuidService())->success('gamertag');
+
+        Http::fakeSequence()
+            ->push($mockXuidResponse, Response::HTTP_INTERNAL_SERVER_ERROR);
+
+        $player = Player::factory()->createOne(['xuid' => null]);
+
+        // Act
+        PullXuid::dispatchSync($player);
+
+        // Assert
+        $this->assertDatabaseHas('players', [
+            'id' => $player->id,
+            'xuid' => null
+        ]);
+    }
+}
