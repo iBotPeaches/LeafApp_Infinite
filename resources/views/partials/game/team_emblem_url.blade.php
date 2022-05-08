cfc046b43588322ef3d4b76932e9429c75e663b4
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-05-08 13:44:36 -0400

    fix: add observer logo for FFA until logo loads

--- resources/views/partials/game/team_emblem_url.blade.php
+++ resources/views/partials/game/team_emblem_url.blade.php
@@ -4,6 +4,8 @@
 @if (empty($gamePlayer->player->emblem_url))
     @if ($gamePlayer->team)
         <img class="is-rounded has-background-dark" src="{{ $gamePlayer->team->emblem_url }}">
+    @else
+        <img class="is-rounded has-background-dark" src="{{ asset('images/teams/8.png') }}">
     @endif
 @else
     <img src="{{ $gamePlayer->player->emblem_url }}">
