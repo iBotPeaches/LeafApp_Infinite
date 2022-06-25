b401a718107ab1c98bcced2f193baa166a1ab979
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-06-25 16:08:52 -0400

    fix: only queue 1 job of xuid

--- app/Observers/PlayerObserver.php
+++ app/Observers/PlayerObserver.php
@@ -5,13 +5,17 @@ namespace App\Observers;
 
 use App\Jobs\PullXuid;
 use App\Models\Player;
+use Illuminate\Support\Facades\Cache;
 
 class PlayerObserver
 {
     public function saved(Player $player): void
     {
-        if (empty($player->xuid)) {
+        $cacheKey = 'xuid-pull-' . $player->id;
+
+        if (empty($player->xuid) && Cache::missing($cacheKey)) {
             PullXuid::dispatch($player);
+            Cache::put($cacheKey, time(), now()->addDays(30));
         }
     }
 }
