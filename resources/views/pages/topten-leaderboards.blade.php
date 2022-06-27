2fdff16a30db0a17bc925bbb9a453f5ea305ae01
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2022-06-27 06:54:26 -0400

    Stat fixes (#425)
    * feat: add feedback collection unit
    
    * feat: drop count to 8 limit on homepage

--- resources/views/pages/topten-leaderboards.blade.php
+++ resources/views/pages/topten-leaderboards.blade.php
@@ -4,4 +4,23 @@
 
 @section('content')
     <livewire:top-ten-table></livewire:top-ten-table>
+    <article class="message is-dark">
+        <div class="message-header">
+            <p>Have an idea for a stat? Let me know</p>
+        </div>
+        <div class="message-body">
+            Submit a <a
+                target="_blank"
+                href="https://github.com/iBotPeaches/LeafApp_Infinite/issues"
+                rel="noreferrer">GitHub</a> issue,
+            <a
+                target="_blank"
+                href="https://twitter.com/iBotPeaches"
+                rel="noreferrer">tweet me</a> or
+            <a
+                target="_blank"
+                href="https://discordapp.com/users/iBotPeaches#1569"
+                rel="noreferrer">discord me</a>.
+        </div>
+    </article>
 @endsection
