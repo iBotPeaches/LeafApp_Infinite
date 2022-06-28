05d9d5fb2018a26d85e57afd0f09f78a687b1ce9
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-06-28 06:58:11 -0400

    feat: add "is_cheater" to Player

---
+++ database/migrations/2022_06_27_232325_add_is_cheater_to_players.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->boolean('is_cheater')->default(false)->after('is_bot');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropColumn('is_cheater');
+        });
+    }
+};
