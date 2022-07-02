d52a46a819f50fcf215d7858dce9b67a00fa31ef
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-07-02 07:31:16 -0400

    fix: correct import for fontawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
 import './bootstrap'
 import './partials/navbar';
 import './partials/dates';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/brands';
