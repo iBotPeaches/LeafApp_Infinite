d0b0a90bceede1640804386815faa52d1e8459c8
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2022-07-22 05:52:48 -0400

    feat: upgrade to autocode 1.4.2 (#454)

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
         'disabled' => env('AUTOCODE_DISABLED', false),
         'key' => env('AUTOCODE_KEY'),
         'domain' => env('AUTOCODE_DOMAIN', 'https://halo.api.stdlib.com'),
-        'version' => env('AUTOCODE_VERSION', '1.4.1'),
+        'version' => env('AUTOCODE_VERSION', '1.4.2'),
         'cooldown' => env('AUTOCODE_COOLDOWN', 120),
         'competitive' => [
             'season' => env('AUTOCODE_CURRENT_SEASON', 2),
