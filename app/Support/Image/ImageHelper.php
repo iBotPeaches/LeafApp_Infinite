0248da10eeb657436bc59f31abb7a40467e8d47f
Connor Tumbleson <connor.tumbleson@gmail.com>
2022-11-10 06:25:30 -0500

    style: ignore phpstan warning

--- app/Support/Image/ImageHelper.php
+++ app/Support/Image/ImageHelper.php
@@ -14,6 +14,7 @@ class ImageHelper
 
         $hash = $result['hash'] ?? null;
 
+        // @phpstan-ignore-next-line
         $filepath = base64_decode((string)$hash);
         return $filepath ? Str::afterLast($filepath, '/') : null;
     }
