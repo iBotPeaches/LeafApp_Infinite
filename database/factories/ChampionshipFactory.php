e231693dd07ea67d1df635b79c337aad81c311b3
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-02-19 06:17:24 -0500

    test: fix factories for faceit

--- database/factories/ChampionshipFactory.php
+++ database/factories/ChampionshipFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Enums\FaceItStatus;
 use App\Models\Championship;
 use App\Services\FaceIt\Enums\Region;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -18,7 +19,9 @@ class ChampionshipFactory extends Factory
         return [
             'faceit_id' => $this->faker->unique()->uuid,
             'name' => $this->faker->word,
+            'description' => $this->faker->paragraph,
             'region' => Region::getRandomValue(),
+            'status' => FaceItStatus::getRandomValue(),
             'started_at' => now(),
         ];
     }
