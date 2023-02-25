779bfdd37adce3a1cf6207c37756af6130aeebca
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-02-25 07:22:27 -0500

    build: remove vue component ref + axios/lodash (#558)

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             'resources/js/app.js',
         ]),
         purge({
-            templates: ['blade', 'vue'],
+            templates: ['blade'],
             safelist: {
                 greedy: [
                     /orange/,
