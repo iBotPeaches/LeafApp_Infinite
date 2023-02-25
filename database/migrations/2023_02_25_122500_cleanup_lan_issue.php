ea4389b1b5abcfbc3aea860aaf59237a64367d5a
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-02-25 07:46:51 -0500

    build: adjust db to handle null is_lan

---
+++ database/migrations/2023_02_25_122500_cleanup_lan_issue.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::table('players')
+            ->whereNotNull('last_lan_game_id_pulled')
+            ->update(['last_lan_game_id_pulled' => null]);
+
+        DB::statement('ALTER TABLE `games` MODIFY `is_lan` TINYINT(1) unsigned null;');
+
+        DB::table('games')
+            ->where('is_lan', false)
+            ->update(['is_lan' => null]);
+    }
+
+    public function down(): void
+    {
+        // This is intentionally blank and not possible to revert.
+    }
+};
