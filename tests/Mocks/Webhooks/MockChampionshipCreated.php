48c6037037207c5b084acbdde097a2dbf3da6bbc
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-03-27 06:44:45 -0400

    feat: FaceIt Webhook: 'championship_created' (#631)

---
+++ tests/Mocks/Webhooks/MockChampionshipCreated.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Mocks\Webhooks;
+
+use App\Services\FaceIt\Enums\WebhookEvent;
+use Tests\Mocks\BaseMock;
+
+class MockChampionshipCreated extends BaseMock
+{
+    public function error(): array
+    {
+        return [];
+    }
+
+    public function success(): array
+    {
+        return [
+            'transaction_id' => $this->faker->uuid,
+            'event' => WebhookEvent::CHAMPIONSHIP_CREATED,
+            'event_id' => $this->faker->uuid,
+            'third_party_id' => $this->faker->uuid,
+            'app_id' => $this->faker->uuid,
+            'timestamp' => now()->toIso8601ZuluString(),
+            'retry_count' => 0,
+            'version' => 1,
+            'payload' => [
+                'id' => '1-'.$this->faker->uuid,
+                'organizer_id' => $this->faker->uuid,
+                'game' => 'halo_infinite',
+            ],
+        ];
+    }
+}
