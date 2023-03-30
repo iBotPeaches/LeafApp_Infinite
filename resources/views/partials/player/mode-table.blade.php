28ec1b9c930c8113de130fee78c2bb4b42239809
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-03-30 06:35:59 -0400

    fix: support no mode data (#648)

--- resources/views/partials/player/mode-table.blade.php
+++ resources/views/partials/player/mode-table.blade.php
@@ -1,18 +1,29 @@
-<table class="table is-striped is-hoverable is-fullwidth">
-    <thead>
-        <tr>
-            <th>Map/Mode</th>
-            <th>% {{ $title }}</th>
-            <th>Total</th>
-        </tr>
-    </thead>
-    @foreach ($mode as $game)
-        <tr>
-            <td>
-                {{ $game->map->name }} {{ $game->category->name }}
-            </td>
-            <td>{{ number_format($game->percentWon, 2) }}%</td>
-            <td>{{ $game->summedTotal }}</td>
-        </tr>
-    @endforeach
-</table>
+@if ($mode->count() === 0)
+    <article class="message is-warning">
+        <div class="message-header">
+            <p>Uh oh</p>
+        </div>
+        <div class="message-body">
+            Not enough ranked played to render :(
+        </div>
+    </article>
+@else
+    <table class="table is-striped is-hoverable is-fullwidth">
+        <thead>
+            <tr>
+                <th>Map/Mode</th>
+                <th>% {{ $title }}</th>
+                <th>Total</th>
+            </tr>
+        </thead>
+        @foreach ($mode as $game)
+            <tr>
+                <td>
+                    {{ $game->map->name }} {{ $game->category->name }}
+                </td>
+                <td>{{ number_format($game->percentWon, 2) }}%</td>
+                <td>{{ $game->summedTotal }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endif
