0d05089a3ddf4ab34d81b038ec5b066acf545532
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-03-31 19:43:04 -0400

    style: linting

--- app/Console/Commands/RefreshAnalytics.php
+++ app/Console/Commands/RefreshAnalytics.php
@@ -28,11 +28,11 @@ class RefreshAnalytics extends Command
             $analyticClass = new $analytic();
 
             $startTime = time();
-            $this->output->writeln('Processing: ' . $analyticClass->title());
+            $this->output->writeln('Processing: '.$analyticClass->title());
 
             ProcessAnalytic::dispatchSync($analyticClass);
 
-            $this->output->writeln('Processed in ' . (time() - $startTime) . ' seconds.');
+            $this->output->writeln('Processed in '.(time() - $startTime).' seconds.');
         }
 
         return CommandAlias::SUCCESS;
