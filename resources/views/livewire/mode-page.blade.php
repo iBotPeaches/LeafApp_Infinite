44a3d51df88fa7232d6e3cec6ebb5e9ba7a27f77
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-04-14 19:18:26 -0400

    fix: warning on mode page

--- resources/views/livewire/mode-page.blade.php
+++ resources/views/livewire/mode-page.blade.php
@@ -24,6 +24,12 @@
         </article>
     </div>
 </div>
+<div class="notification is-danger">
+    If you are viewing this - you tricked the URL. This is disabled because the migration towards a new API has required
+    a re-classification of ALL game data to properly know the category (gametype).
+
+    This is 31 million games soooo it'll take a bit to catch up.
+</div>
 <div class="notification is-light mt-2">
     Tracking Ranked with requiring a minimum of 5 matches for a map/mode to be included.
 </div>
