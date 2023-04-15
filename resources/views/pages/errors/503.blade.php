7c96baeb2663e93efdcdcbcf452e6ca8ce694dac
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-04-15 11:22:55 -0400

    feat: deploy page (#676)

---
+++ resources/views/pages/errors/503.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Leaf - Doing Upgrades')
+
+@section('content')
+    <div class="notification is-warning">
+        An upgrade is in progress. Check back soon.
+    </div>
+    <div class="notification">
+        ETA: 3-5pm Eastern - April 15, 2023
+    </div>
+@endsection
