d5bd9d0c2b21a051be1bcdb50b1eb2a77a48e81f
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-04-18 06:53:10 -0400

    feat: ui layer for bans

---
+++ resources/views/partials/player/ban-header.blade.php
@@ -0,0 +1,14 @@
+<?php
+    /** @var App\Models\Player $player */
+?>
+@if ($player->bans)
+    @foreach ($player->bans as $ban)
+        <div class="notification is-danger">
+            {{ $ban->message }}
+            <br /><br />
+            <p class="is-size-7">
+                Days remaining: {{ number_format($ban->ends_at->diffInDays()) }}, for '{{ $ban->type }}', on scope: '{{ $ban->scope }}'
+            </p>
+        </div>
+    @endforeach
+@endif
