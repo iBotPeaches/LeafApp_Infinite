5ac2741604ee97a27338583bd84ac4290133047e
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-04-26 21:04:54 -0400

    fix: remove etag due to collision on info (#722)

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::post('/webhooks/faceit', FaceItController::class)->name('webhooks.faceit'
 Route::redirect('sitemap.xml', '/sitemaps/sitemap.xml', Response::HTTP_PERMANENTLY_REDIRECT);
 
 // Leaderboard
-Route::middleware('cache.headers:public;max_age=21600;etag')->group(function () {
+Route::middleware('cache.headers:public;max_age=21600')->group(function () {
     Route::get('/leaderboards/top-ten', [LeaderboardController::class, 'topTenList'])->name('topTenLeaderboards');
     Route::get('/leaderboards/top-ten/{key}', [LeaderboardController::class, 'topTen'])->name('topTenLeaderboard');
     Route::get('/leaderboards/medal', [LeaderboardController::class, 'medalList'])->name('medalLeaderboards');
@@ -55,7 +55,7 @@ Route::get('/game/{game}/csv', [GameController::class, 'csv'])->name('gameCsv');
 Route::get('/game/{game}', [GameController::class, 'index'])->name('game');
 
 // HCS
-Route::middleware('cache.headers:public;max_age=300;etag')->group(function () {
+Route::middleware('cache.headers:public;max_age=300')->group(function () {
     Route::get('/hcs/{championship}/matchup/{matchup}', [HcsController::class, 'matchup'])->name('matchup');
     Route::get('/hcs/{championship}/{bracket?}/{round?}', [HcsController::class, 'championship'])->name('championship');
     Route::get('/hcs', [HcsController::class, 'index'])->name('championships');
