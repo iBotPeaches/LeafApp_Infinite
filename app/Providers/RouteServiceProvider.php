4db94356085b8071d9e085e6e8012ed8252297cb
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-04-30 07:47:15 -0400

    feat: add rate limit for csv exports (#729)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -23,5 +26,13 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        // @codeCoverageIgnoreStart
+        RateLimiter::for('uploads', function () {
+            return Limit::perHour(1)->response(function () {
+                return response()->view('pages.errors.429', [], Response::HTTP_TOO_MANY_REQUESTS);
+            });
+        });
+        // @codeCoverageIgnoreEnd
     }
 }
