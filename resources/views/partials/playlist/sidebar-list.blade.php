96d2c6674a843d0f672d1816615c527fd95b0617
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-05-09 20:20:40 -0400

    feat: swap sidebar to blue (info)

--- resources/views/partials/playlist/sidebar-list.blade.php
+++ resources/views/partials/playlist/sidebar-list.blade.php
@@ -2,12 +2,12 @@
     /** @var App\Models\Playlist[] $playlists */
     /** @var App\Models\Playlist $playlist */
 ?>
-<article class="panel is-primary">
+<article class="panel is-info">
     <p class="panel-heading">
         Playlists
     </p>
     @foreach ($playlists as $p)
-        <a class="panel-block is-block <?= $p->id === $playlist->id ? 'is-bold is-active' : ''; ?>"
+        <a class="panel-block is-block <?= $p->id === $playlist->id ? 'has-background-link-light' : ''; ?>"
            href="<?= route('playlist', [$p]); ?>"
         >
             {{ $p->name }}
