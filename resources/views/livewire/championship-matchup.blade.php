3a0eeb91286b446ac3af3f1c0c94d2f3df36680a
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-08-29 19:25:52 -0400

    fix: don't crash if no bracket name (#957)

--- resources/views/livewire/championship-matchup.blade.php
+++ resources/views/livewire/championship-matchup.blade.php
@@ -6,7 +6,7 @@
 ?>
 <h1 class="title">{{ $championship->name }}</h1>
 <h2 class="subtitle">
-    {{ $matchup->bracket->description }} Round {{ $matchup->round }}
+    {{ $matchup->bracket?->description }} Round {{ $matchup->round }}
     (<a target="_blank" href="{{ $matchup->faceitUrl }}" rel="nofollow">FaceIt</a>)
 </h2>
 @include('partials.hcs.matchup_breakdown')
