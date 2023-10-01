0e79118aaa413b1c3141c85e0502167d07b26645
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-10-01 13:19:40 -0400

    test: add tests for game badges

---
+++ tests/Feature/Forms/GameBadges/AnalyticGameBadgeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Forms\GameBadges;
+
+use App\Http\Livewire\GameBadges;
+use App\Models\Analytic;
+use App\Models\Game;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class AnalyticGameBadgeTest extends TestCase
+{
+    public function testLoadingAnalyticsOnBadges(): void
+    {
+        // Arrange
+        $game = Game::factory()->createOne();
+
+        Analytic::factory()->createOne([
+            'game_id' => $game->id,
+        ]);
+
+        // Act & Assert
+        Livewire::test(GameBadges::class, [
+            'game' => $game,
+        ])
+            ->call('loadBadges')
+            ->assertSuccessful();
+    }
+}
---
+++ tests/Feature/Forms/GameBadges/EmptyGameBadgeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Forms;
+
+use App\Http\Livewire\GameBadges;
+use App\Models\Game;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class EmptyGameBadgeTest extends TestCase
+{
+    public function testLoadGameBadgesWithNothing(): void
+    {
+        // Arrange
+        $game = Game::factory()->createOne();
+
+        // Act & Assert
+        Livewire::test(GameBadges::class, [
+            'game' => $game,
+        ])
+            ->call('loadBadges')
+            ->assertSuccessful();
+    }
+}
