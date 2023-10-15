8b85a354762e8e04637fb8181fcde335919cdaeb
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-10-14 22:34:00 -0400

    fix: temporarily remove modes tab
     - its causing insane server load

--- resources/views/partials/player/navigation.blade.php
+++ resources/views/partials/player/navigation.blade.php
@@ -22,12 +22,12 @@
                 <span>Competitive</span>
             </a>
         </li>
-        <li class="<?= $type === App\Enums\PlayerTab::MODES ? 'is-active' : null; ?>">
+        <!--<li class="<?= $type === App\Enums\PlayerTab::MODES ? 'is-active' : null; ?>">
             <a href="<?= route('player', [$player, App\Enums\PlayerTab::MODES]); ?>">
                 <span class="icon"><i class="fa fa-chart-simple"></i></span>
                 <span>Modes</span>
             </a>
-        </li>
+        </li>-->
         <li class="<?= $type === App\Enums\PlayerTab::MATCHES ? 'is-active' : null; ?>">
             <a href="<?= route('player', [$player, App\Enums\PlayerTab::MATCHES]); ?>">
                 <span class="icon"><i class="fa fa-history"></i></span>
