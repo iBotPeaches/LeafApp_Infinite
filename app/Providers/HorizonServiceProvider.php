f8d818c539e3cb8b3194ae4becb35b5f8348fa6a
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-12-15 06:33:54 -0500

    fix: remove horizon:night mode (#1095)

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -5,18 +5,10 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
-use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
-    public function boot(): void
-    {
-        parent::boot();
-
-        Horizon::night();
-    }
-
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
