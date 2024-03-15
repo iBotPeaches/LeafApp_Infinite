e3e33f20c217ae8cae4e06a5eb120d829564305f
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-03-15 06:09:13 -0400

    wip: progress to v5 upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import purge from '@erbelion/vite-plugin-laravel-purgecss';
 import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
@@ -12,31 +11,6 @@ export default defineConfig({
             'resources/sass/app.scss',
             'resources/js/app.js',
         ]),
-        purge({
-            templates: ['blade'],
-            safelist: {
-                greedy: [
-                    /orange/,
-                    /purple/,
-                    /unranked/,
-                ],
-                deep: [
-                    /has-tooltip-info/,
-                    /has-tooltip-success/,
-                    /has-tooltip-warning/,
-                    /has-tooltip-danger/,
-                    /has-text-success/,
-                    /has-text-info/,
-                    /has-text-warning/,
-                    /has-text-danger/,
-                    /has-text-primary/,
-                    /has-background-success-light/,
-                    /has-background-info-light/,
-                    /has-background-warning-light/,
-                    /has-background-danger-light/,
-                ]
-            },
-        }),
         manifestSRI(),
     ],
 });
