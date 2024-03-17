e9e5b494329efaa706a62587831e8465a5c36488
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-03-17 16:57:55 -0400

    chore: move towards fixed-grid/grid instead of tile

--- resources/views/livewire/medals-page.blade.php
+++ resources/views/livewire/medals-page.blade.php
@@ -16,40 +16,42 @@
         @if ($player->is_private)
             @include('partials.global.account_private')
         @else
-            @foreach ($medals as $medal)
-                <article class="tile is-flex">
-                    <figure class="media-left">
-                        <p class="image is-48x48">
-                            <img
-                                class="{{ $medal->count === 0 ? 'is-greyscale' : '' }}"
-                                src="{{ $medal->image }}"
-                                alt="{{ $medal->name }}"
-                            />
-                        </p>
-                    </figure>
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-                                <a
-                                    href="{{ route('medalLeaderboard', [$medal]) }}"
-                                    class="{{ $medal->text_color }}"
-                                >
-                                    <strong style="white-space: nowrap">
-                                        {{ $medal->name }}
-                                    </strong>
-                                </a>
-                                <br>
-                                {{ $medal->description }}
+            <div class="fixed-grid has-1-cols">
+                @foreach ($medals as $medal)
+                    <article class="cell media">
+                        <figure class="media-left">
+                            <p class="image is-48x48">
+                                <img
+                                    class="{{ $medal->count === 0 ? 'is-greyscale' : '' }}"
+                                    src="{{ $medal->image }}"
+                                    alt="{{ $medal->name }}"
+                                />
                             </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <a
+                                        href="{{ route('medalLeaderboard', [$medal]) }}"
+                                        class="{{ $medal->text_color }}"
+                                    >
+                                        <strong style="white-space: nowrap">
+                                            {{ $medal->name }}
+                                        </strong>
+                                    </a>
+                                    <br>
+                                    {{ $medal->description }}
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="media-right">
-                    <span class="tag {{ $medal->count === 0 ? 'is-light' : 'is-dark' }}">
-                        {{ number_format($medal->count) }}
-                    </span>
-                    </div>
-                </article>
-            @endforeach
+                        <div class="media-right">
+                            <span class="tag {{ $medal->count === 0 ? 'is-light' : 'is-dark' }}">
+                                {{ number_format($medal->count) }}
+                            </span>
+                        </div>
+                    </article>
+                @endforeach
+            </div>
         @endif
     @endif
 </div>
