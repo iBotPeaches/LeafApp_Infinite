cd7523639f6ebf0045f697ffc9819a133681aae2
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-03-17 16:57:55 -0400

    chore: move towards fixed-grid/grid instead of tile

--- resources/views/livewire/medals-table.blade.php
+++ resources/views/livewire/medals-table.blade.php
@@ -14,8 +14,8 @@
             <tbody>
             @foreach ($medals as $medal)
                 <tr>
-                    <td>
-                        <article class="tile is-flex">
+                    <td class="grid">
+                        <article class="cell is-flex">
                             <figure class="media-left">
                                 <p class="image is-48x48">
                                     <img src="{{ $medal->image }}" alt="{{ $medal->name }}" />
--- resources/views/livewire/player-overview-page.blade.php
+++ resources/views/livewire/player-overview-page.blade.php
@@ -11,9 +11,10 @@
         @else
             <div class="notification is-warning">
                 <p>
-                    No record was found for this user for this specific season: <strong>{{ $season->name ?? 'n/a' }}</strong>.
+                    No record was found for this user for this specific season:
+                    <strong>{{ $season->name ?? 'n/a' }}</strong>.
                 </p>
-                <br /><br />
+                <br/><br/>
                 <p class="is-size-7">
                     If this is not intended - just click "Request Stat Update"
                 </p>
@@ -31,12 +32,13 @@
         @else
             @if ($mode->is(\App\Enums\Mode::MATCHMADE_RANKED()) && !$isAllSeasons)
                 <div class="notification is-warning">
-                    We currently cannot pull filtered data (ie ranked) from a specific season. If you have data it's from an older Leaf that could.
+                    We currently cannot pull filtered data (ie ranked) from a specific season. If you have data it's
+                    from an older Leaf that could.
                 </div>
             @endif
             @include('partials.player.stats')
             @if ($serviceRecord->medals)
-                @include('partials.player.medal-groups')
+                @include('partials.player.medals')
             @endif
         @endif
     @endif
