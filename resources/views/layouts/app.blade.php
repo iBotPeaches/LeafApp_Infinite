24ea9c61f9877210c7b677b6f61b8529c2ab025d
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2024-04-04 06:38:05 -0400

    fix: let livewire load itself (#1228)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     {!! Artesaos\SEOTools\Facades\SEOTools::generate() !!}
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <livewire:styles />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -20,6 +19,5 @@
         @yield('content')
     </main>
     @include('partials.global.footer')
-    <livewire:scripts />
 </body>
 </html>
