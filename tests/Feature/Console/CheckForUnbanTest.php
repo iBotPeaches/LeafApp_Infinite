d010d1fc399a6c45480e6b6f9173e1349ee0d716
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-05 19:39:23 -0400

    test: 100% coverage for unban

---
+++ tests/Feature/Console/CheckForUnbanTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Console;
+
+use App\Models\Player;
+use App\Models\PlayerBan;
+use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+use Tests\TestCase;
+
+class CheckForUnbanTest extends TestCase
+{
+    use WithFaker;
+
+    public function testUnbanning(): void
+    {
+        // Arrange
+        $playerBan = PlayerBan::factory()
+            ->for(Player::factory()->state(['is_cheater' => true]))
+            ->createOne([
+                'ends_at' => now()->subDay(),
+            ]);
+
+        // Act & Assert
+        $this
+            ->artisan('app:check-for-unban')
+            ->assertExitCode(CommandAlias::SUCCESS)
+            ->expectsOutput("Player {$playerBan->player->gamertag} has been unbanned.");
+    }
+
+    public function testUnbanFailsOnActiveCheater(): void
+    {
+        // Arrange
+        $playerBan = PlayerBan::factory()
+            ->for(Player::factory()->state(['is_cheater' => true]))
+            ->createOne([
+                'ends_at' => now()->addDay(),
+            ]);
+
+        // Act & Assert
+        $this
+            ->artisan('app:check-for-unban')
+            ->assertExitCode(CommandAlias::SUCCESS)
+            ->doesntExpectOutput("Player {$playerBan->player->gamertag} has been unbanned.");
+    }
+}
