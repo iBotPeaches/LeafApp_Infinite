36fd396573ecc1f9567b23319064826b77df5682
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-18 06:59:52 -0400

    feat: image shortcut for map image

--- app/Models/Overview.php
+++ app/Models/Overview.php
@@ -9,14 +9,17 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 /**
  * @property int $id
  * @property string $name
  * @property string $slug
- * @property string $image
+ * @property string $thumbnail_url
  * @property Carbon $created_at
  * @property Carbon $updated_at
+ * @property-read string $image
  * @property-read Collection<OverviewGametype> $gametypes
  * @property-read Collection<OverviewMap> $maps
  * @property-read Collection<OverviewStat> $stats
@@ -28,6 +31,17 @@ class Overview extends Model
     public $guarded = [
         'id',
     ];
+
+    public function getImageAttribute(): string
+    {
+        $filename = Str::slug($this->name).'.jpg';
+
+        if (File::exists(public_path('images/maps/'.$filename))) {
+            return asset('images/maps/'.$filename);
+        }
+
+        return $this->thumbnail_url;
+    }
 
     public function gametypes(): HasMany
     {
