1540d34fb4e69e1c29c8c48f359876253e649357
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-19 20:07:20 -0400

    fix: add layer of overview stats

--- resources/views/partials/overview/overview-stats.blade.php
+++ resources/views/partials/overview/overview-stats.blade.php
@@ -34,3 +34,44 @@
         </div>
     </div>
 </div>
+<div class="divider">Overall</div>
+<div class="level">
+    <div class="level-item has-text-centered">
+        <div>
+            <p class="heading">Total Kills</p>
+            <p class="title">
+                {{ number_format($overviewStat->total_kills) }}
+            </p>
+        </div>
+    </div>
+    <div class="level-item has-text-centered">
+        <div>
+            <p class="heading">Total Deaths</p>
+            <p class="title">
+                {{ number_format($overviewStat->total_deaths) }}
+            </p>
+        </div>
+    </div>
+    <div class="level-item has-text-centered">
+        <div>
+            <p class="heading">Total Assists</p>
+            <p class="title">{{ number_format($overviewStat->total_assists) }}</p>
+        </div>
+    </div>
+    <div class="level-item has-text-centered">
+        <div>
+            <p class="heading">Total Suicides</p>
+            <p class="title">
+                {{ number_format($overviewStat->total_suicides) }}
+            </p>
+        </div>
+    </div>
+    <div class="level-item has-text-centered">
+        <div>
+            <p class="heading">Total Medals</p>
+            <p class="title">
+                {{ number_format($overviewStat->total_medals) }}
+            </p>
+        </div>
+    </div>
+</div>
