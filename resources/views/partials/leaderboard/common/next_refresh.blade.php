46bd5dae28380405a355c5484681a407531c6ac4
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2024-04-21 16:23:55 -0400

    fix: align margin 2 (#1269)

--- resources/views/partials/leaderboard/common/next_refresh.blade.php
+++ resources/views/partials/leaderboard/common/next_refresh.blade.php
@@ -2,7 +2,7 @@
 /** @var Carbon\Carbon $nextDate */
 ?>
 @if ($nextDate)
-    <div class="notification is-light mt-2">
+    <div class="notification is-light mb-2">
         Next refresh: <i>{{ $nextDate->diffForHumans() }}</i>
     </div>
 @endif
