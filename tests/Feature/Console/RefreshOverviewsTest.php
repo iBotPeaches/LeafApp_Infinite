e9c3295aae1234d7d72e09240db43154ed3e87c4
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-21 15:08:20 -0400

    test: assert 0 matches found

--- tests/Feature/Console/RefreshOverviewsTest.php
+++ tests/Feature/Console/RefreshOverviewsTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Tests\Feature\Console;
 
 use App\Models\Game;
+use App\Models\Gamevariant;
+use App\Models\Level;
+use App\Models\Map;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
@@ -15,7 +18,28 @@ class RefreshOverviewsTest extends TestCase
     public function testValidGenerationOfOverviews(): void
     {
         // Arrange
-        $game = Game::factory()->createOne();
+        $level = Level::factory()->createOne();
+        $gametype1 = Gamevariant::factory()->createOne();
+        $gametype2 = Gamevariant::factory()->createOne();
+
+        $map1 = Map::factory()->createOne([
+            'name' => 'Absolute',
+            'level_id' => $level->id,
+        ]);
+        $map2 = Map::factory()->createOne([
+            'name' => 'Absolute',
+            'level_id' => $level->id,
+        ]);
+
+        $game = Game::factory()->createOne([
+            'map_id' => $map1->id,
+            'gamevariant_id' => $gametype1->id,
+        ]);
+
+        $game2 = Game::factory()->createOne([
+            'map_id' => $map2->id,
+            'gamevariant_id' => $gametype2->id,
+        ]);
 
         // Act
         $this->artisan('analytics:overviews:refresh')
