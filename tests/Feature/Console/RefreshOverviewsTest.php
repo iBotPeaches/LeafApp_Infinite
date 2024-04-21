5926f32b0ce27f5c30699a6406bcd0a71e6abb0b
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-21 15:19:57 -0400

    test: assertion for invalid base gametype

--- tests/Feature/Console/RefreshOverviewsTest.php
+++ tests/Feature/Console/RefreshOverviewsTest.php
@@ -21,6 +21,18 @@ class RefreshOverviewsTest extends TestCase
         $level = Level::factory()->createOne();
         $gametype1 = Gamevariant::factory()->createOne();
         $gametype2 = Gamevariant::factory()->createOne();
+        $gametypeKoth = Gamevariant::factory()->createOne([
+            'name' => 'King of the Hill',
+        ]);
+        $gametypeLSS = Gamevariant::factory()->createOne([
+            'name' => 'Last Spartan Standing',
+        ]);
+        $gametypeDodgeball = Gamevariant::factory()->createOne([
+            'name' => 'Dodgeball',
+        ]);
+        $gametypeSlayholds = Gamevariant::factory()->createOne([
+            'name' => 'Slayholds',
+        ]);
 
         $map1 = Map::factory()->createOne([
             'name' => 'Absolute',
@@ -36,13 +48,53 @@ class RefreshOverviewsTest extends TestCase
             'gamevariant_id' => $gametype1->id,
         ]);
 
-        $game2 = Game::factory()->createOne([
+        Game::factory()->createOne([
             'map_id' => $map2->id,
             'gamevariant_id' => $gametype2->id,
         ]);
 
+        Game::factory()->createOne([
+            'map_id' => $map1->id,
+            'gamevariant_id' => $gametypeKoth->id,
+        ]);
+
+        Game::factory()->createOne([
+            'map_id' => $map1->id,
+            'gamevariant_id' => $gametypeLSS->id,
+        ]);
+
+        Game::factory()->createOne([
+            'map_id' => $map1->id,
+            'gamevariant_id' => $gametypeDodgeball->id,
+        ]);
+
+        Game::factory()->createOne([
+            'map_id' => $map1->id,
+            'gamevariant_id' => $gametypeSlayholds->id,
+        ]);
+
         // Act
         $this->artisan('analytics:overviews:refresh')
-            ->expectsOutputToContain('Processed '.$game->map->name);
+            ->expectsOutputToContain('Processed '.$game->map->name)
+            ->assertOk();
+    }
+
+    public function testUnknownGametype(): void
+    {
+        // Expectations
+        $this->expectExceptionMessage('Unable to find base gametype for: not a real gametype');
+
+        // Arrange
+        $gametype = Gamevariant::factory()->createOne([
+            'name' => 'not a real gametype',
+        ]);
+
+        Game::factory()->createOne([
+            'gamevariant_id' => $gametype->id,
+        ]);
+
+        // Act
+        $this->artisan('analytics:overviews:refresh')
+            ->assertFailed();
     }
 }
