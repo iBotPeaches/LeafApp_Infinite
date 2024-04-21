17666d8a27305be16b3659e6e3093e827d9a258b
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-21 10:12:50 -0400

    feat: add seo for overview.show

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Enums\OverviewTab;
 use App\Models\Overview;
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\View\View;
 
 class OverviewController extends Controller
@@ -17,6 +18,12 @@ class OverviewController extends Controller
 
     public function show(Overview $overview, string $tab = OverviewTab::OVERVIEW): View
     {
+        SEOTools::setTitle('Map Overview: '.$overview->name);
+        SEOTools::addImages([
+            $overview->image,
+        ]);
+        SEOTools::setDescription('Halo Infinite - Leaf Map Overview: '.$overview->name);
+
         return view('pages.overview', [
             'overview' => $overview,
             'tab' => $tab,
