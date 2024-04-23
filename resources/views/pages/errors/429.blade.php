4a6c3f50c86148b967c91f4482b0ceba522654dd
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2024-04-22 20:16:31 -0400

    chore: adjust rate limit message (#1279)
    * chore: adjust rate limit message
    
    * chore: grammar

--- resources/views/pages/errors/429.blade.php
+++ resources/views/pages/errors/429.blade.php
@@ -7,11 +7,18 @@
             <p>Whoa whoa whoa</p>
         </div>
         <div class="message-body">
-            Listen, I lose money on this site monthly. I pay $75/month ($40 server, $35 api access). This action you did
+            I lose money on this site monthly. I pay $75/month ($40 server, $35 api access). This action you did
             takes a lot of resources on this server. Quite recently it has been hammered costing me more money.
             <br />
             <br />
-            So I have to rate limit it heavily to once per hour.
+            You are welcome to donate to help offset these costs. Leave me your gamertag, and you'll earn the <strong>Leaf badge</strong>
+            <span class="tag is-success" data-tooltip="Donated via BuyMeACoffee" style="border-bottom: 0;">
+                <i class="fas fa-leaf"></i>
+            </span>
+            on the site - <a href="https://www.buymeacoffee.com/iBotPeaches" target="_blank">(donation link)</a>.
+            <br />
+            <br />
+            So in short - I have to rate limit it heavily to 10 exports per hour per user.
         </div>
     </article>
 @endsection
