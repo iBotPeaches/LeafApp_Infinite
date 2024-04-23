6e5a51add67f9b22608f9b3c74afee8cee991952
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-22 20:02:47 -0400

    chore: adjust rate limit message

--- resources/views/pages/errors/429-ban.blade.php
+++ resources/views/pages/errors/429-ban.blade.php
@@ -10,6 +10,13 @@
             Checking for an account ban is not easy. So I have to restrict this endpoint.
             <br />
             <br />
+            You are welcome to donate to help offset these costs. Leave me your gamertag, and you'll earn the <strong>Leaf badge</strong>
+            <span class="tag is-success" data-tooltip="Donated via BuyMeACoffee" style="border-bottom: 0;">
+                <i class="fas fa-leaf"></i>
+            </span>
+            on the site - <a href="https://www.buymeacoffee.com/iBotPeaches" target="_blank">(donation link)</a>.
+            <br />
+            <br />
             So I have to rate limit it heavily to five per day.
         </div>
     </article>
