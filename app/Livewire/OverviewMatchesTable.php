9415aab5ece7175f91397a75ccde097fcf2e9214
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-22 21:21:50 -0400

    fix: support filtering match browser

--- app/Livewire/OverviewMatchesTable.php
+++ app/Livewire/OverviewMatchesTable.php
@@ -5,6 +5,9 @@ namespace App\Livewire;
 use App\Livewire\Traits\HasScrimEditor;
 use App\Models\Game;
 use App\Models\Overview;
+use App\Models\OverviewGametype;
+use App\Models\OverviewMap;
+use App\Support\Session\OverviewGametypeSession;
 use App\Support\Session\OverviewMapSession;
 use Illuminate\View\View;
 use Livewire\Component;
@@ -17,15 +20,30 @@ class OverviewMatchesTable extends Component
 
     public Overview $overview;
 
+    // @phpstan-ignore-next-line
+    public $listeners = [
+        '$refresh',
+    ];
+
     public function render(): View
     {
-        $mapId = OverviewMapSession::get($this->overview);
+        $overviewMapId = OverviewMapSession::get($this->overview);
+        $overviewGametypeId = OverviewGametypeSession::get($this->overview);
+
+        // The default filter needs to be all maps of this overview
         $mapIds = $this->overview->maps->pluck('map_id')->toArray();
+
+        /** @var OverviewMap|null $filteredMapId */
+        $filteredMapId = $this->overview->maps->firstWhere('id', $overviewMapId);
+
+        /** @var OverviewGametype|null $filteredGametypeIds */
+        $filteredGametypeIds = $this->overview->gametypes->firstWhere('id', $overviewGametypeId);
 
         $games = Game::query()
             ->with(['playlist', 'map', 'category', 'teams'])
-            ->when($mapId === -1, fn ($query) => $query->whereIn('map_id', $mapIds))
-            ->when($mapId !== -1, fn ($query) => $query->where('map_id', $mapId))
+            ->when($overviewMapId === -1, fn ($query) => $query->whereIn('map_id', $mapIds))
+            ->when($overviewMapId !== -1, fn ($query) => $query->where('map_id', $filteredMapId?->map_id))
+            ->when($overviewGametypeId !== -1, fn ($query) => $query->whereIn('gamevariant_id', $filteredGametypeIds?->gamevariant_ids))
             ->orderByDesc('occurred_at')
             ->simplePaginate(16);
 
