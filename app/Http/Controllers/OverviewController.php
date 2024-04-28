33700dde80047e1c9838021f8e671101ce556b63
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-28 15:55:24 -0400

    feat: introduce filter (customs/matchmaking

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Enums\OverviewTab;
+use App\Enums\OverviewType;
 use App\Models\Overview;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\View\View;
 
 class OverviewController extends Controller
 {
-    public function list(): View
+    public function list(string $filterType = OverviewType::MATCHMAKING): View
     {
         SEOTools::setTitle('Map Overviews');
         SEOTools::setDescription('Halo Infinite - Leaf Map Overviews');
 
-        return view('pages.overviews');
+        return view('pages.overviews', [
+            'type' => $filterType,
+        ]);
     }
 
     public function show(Overview $overview, string $tab = OverviewTab::OVERVIEW): View
