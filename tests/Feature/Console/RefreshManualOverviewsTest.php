4f609058cc772ddcb2f96af33efbd777ea0f7fb8
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-04-28 16:31:25 -0400

    test: add assertions for manual refresh

---
+++ tests/Feature/Console/RefreshManualOverviewsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Console;
+
+use App\Models\Overview;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RefreshManualOverviewsTest extends TestCase
+{
+    use WithFaker;
+
+    public function testValidGenerationOfOverviews(): void
+    {
+        // Arrange
+        $overview = Overview::factory()->createOne([
+            'is_manual' => true,
+        ]);
+
+        // Act
+        $this->artisan('analytics:overviews:manual-refresh')
+            ->expectsOutputToContain('Processed '.$overview->name)
+            ->assertOk();
+    }
+}
