4e7483ff80450ef7c9e99a171413897ddd857b81
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2024-05-22 06:51:44 -0400

    fix: remove dependency on api.php (#1336)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,6 @@ use Sentry\Laravel\Integration;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
