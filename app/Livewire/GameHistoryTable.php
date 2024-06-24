1b7a5584dd6b2ca53221a2876759e300dea56f96
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-06-23 20:24:36 -0400

    fix: add playlist filter to matches

--- app/Livewire/GameHistoryTable.php
+++ app/Livewire/GameHistoryTable.php
@@ -6,7 +6,9 @@ namespace App\Livewire;
 
 use App\Livewire\Traits\HasScrimEditor;
 use App\Models\Player;
+use App\Models\Playlist;
 use Illuminate\View\View;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -16,6 +18,9 @@ class GameHistoryTable extends Component
     use WithPagination;
 
     public Player $player;
+
+    #[Url]
+    public ?string $playlist = '';
 
     // @phpstan-ignore-next-line
     public $listeners = [
@@ -30,10 +35,18 @@ class GameHistoryTable extends Component
 
     public function render(): View
     {
+        $playlist = null;
+        if ($this->playlist) {
+            $playlist = Playlist::query()
+                ->where('uuid', $this->playlist)
+                ->first();
+        }
+
         return view('livewire.game-history-table', [
             'isScrimEditor' => $this->isScrimEditor,
             'games' => $this->player
                 ->games()
+                ->when($playlist, fn ($query) => $query->where('playlist_id', $playlist?->id))
                 ->with(['playlist', 'map', 'category'])
                 ->whereNotNull('playlist_id')
                 ->orderByDesc('occurred_at')
