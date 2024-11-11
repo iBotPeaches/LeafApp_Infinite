a727bcc4f0c67cab168318e543c9f2a7d48fa5ee
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2024-11-11 13:07:28 -0500

    Revert "feat: add lvl55 gg callout (#1463)" (#1483)
    This reverts commit 307f26d1c9fb807eec841a14887cc6758afbe036.

--- resources/views/partials/home/h2_lvl55.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="notification is-success mb-2 mt-2">
-    <a target="_blank" href="https://leafapp.co/player/iBot">I</a>
-    love Halo 2 and supporting the upcoming Halo 2 tournament - <a target="_blank" href="https://www.lvl50.gg">LVL50.gg</a>.
-    If you donate and tell me - you'll get the Leaf badge (<i class="fas fa-leaf"></i>) on your profile!
-</div>
