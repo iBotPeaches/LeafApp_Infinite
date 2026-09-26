59aa652ba4dd08016d4710fd500dac72d57ec025
Connor Tumbleson <connor.tumbleson@gmail.com>
2026-09-26 12:51:02 -0400

    fix: support missing player

--- resources/views/partials/links/player.blade.php
+++ resources/views/partials/links/player.blade.php
@@ -1,9 +1,13 @@
 <?php
-    /** @var \App\Models\Player $player */
+    /** @var \App\Models\Player|null $player */
 ?>
-<a
-    href="{{ route('player', [$player]) }}"
-    class="{{ $player?->is_cheater ? 'is-cheater' : '' }} {{ $player?->is_donator ? 'is-donator' : '' }}"
->
-    {{ $player?->gamertag }}
-</a>
+@if ($player)
+    <a
+        href="{{ route('player', [$player]) }}"
+        class="{{ $player->is_cheater ? 'is-cheater' : '' }} {{ $player->is_donator ? 'is-donator' : '' }}"
+    >
+        {{ $player->gamertag }}
+    </a>
+@else
+    <span class="has-text-grey">Unknown</span>
+@endif
